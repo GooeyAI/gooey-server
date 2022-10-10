08e9df46cee6ed4a6abdc1ca61bce5001e6027aa
Dev Aggarwal <devxpy@gmail.com>
2022-10-11 02:33:45 +0530

    add privacy policy

--- daras_ai/logo.py
+++ daras_ai/logo.py
@@ -7,6 +7,7 @@ def logo():
         <a href="/" target="_self">
             <img width=150 src="https://storage.googleapis.com/dara-c1b52.appspot.com/daras_ai/media/logo.webp"></img>
         </a>
+        <a style="position: absolute; right: 0px" href="https://dara.network/privacy/">Privacy Policy</a>
         """,
         unsafe_allow_html=True,
     )
