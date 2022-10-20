db349f2503327b968486987136ac8cc813a9145e
singhmitesh <singhmitesh19@gmail.com>
2022-10-20 12:38:15 +0530

    Add file attachments to send_email

--- send_email.py
+++ send_email.py
@@ -1,14 +1,17 @@
 import smtplib
+from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from os.path import basename
 
 import streamlit as st
 from decouple import config
 
 
 def send_smtp_message(
-        smtp_server, smtp_username, smtp_password, sender, to_address, cc_address,
-        subject, html_message, text_message):
+        smtp_server, smtp_username,
+        smtp_password, sender, to_address, cc_address,
+        subject_text, html_message, text_message, files=None):
     """
     Sends an email by using an Amazon Pinpoint SMTP server.
 
@@ -20,7 +23,7 @@ def send_smtp_message(
                        this address must be verified.
     :param cc_address: The "CC" address. If your account is still in the sandbox,
                        this address must be verified.
-    :param subject: The subject line of the email.
+    :param subject_text: The subject line of the email.
     :param html_message: The HTML body of the email.
     :param text_message: The email body for recipients with non-HTML email clients.
     """
@@ -29,9 +32,16 @@ def send_smtp_message(
     msg['From'] = "events@dara.network"  # sender
     msg['To'] = to_address
     msg['Cc'] = cc_address
-    msg['Subject'] = subject
+    msg['Subject'] = subject_text
     msg.attach(MIMEText(html_message, 'html'))
     msg.attach(MIMEText(text_message, 'plain'))
+    for f in files or []:
+        part = MIMEApplication(
+            f.getvalue(),
+            Name=basename(f.name)
+        )
+        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f.name)
+        msg.attach(part)
 
     smtp_server.ehlo()
     smtp_server.starttls()
@@ -44,12 +54,12 @@ def send_smtp_message(
 
 
 def send_email():
-    print([to_email, from_email])
+    print([to_email, from_email, files])
     try:
         with smtplib.SMTP(config("AWS_SMTP_SERVER"), config("AWS_SMTP_PORT")) as smtp_server:
             send_smtp_message(
                 smtp_server, config("AWS_SMTP_USERNAME"), config("AWS_SMTP_PASSWORD"), from_email, to_email,
-                "", subject, "", body)
+                "", subject, "", body, files=files)
     except Exception:
         st.error(body="Couldn't send message.", icon="⚠️")
         raise
@@ -63,7 +73,9 @@ with st.form(key="send_email", clear_on_submit=False):
     from_email = st.text_input(label="From")
     subject = st.text_input(label="Subject")
     body = st.text_area(label="Body")
-    # st.file_uploader(label="Attachments")
+    files = st.file_uploader(label="Attachments",
+                             accept_multiple_files=True
+                             )
     submitted = st.form_submit_button("Send")
     if submitted:
         send_email()
