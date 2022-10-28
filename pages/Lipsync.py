fb755937c62d18ad554c338b797f7ea222821680
Dev Aggarwal <devxpy@gmail.com>
2022-10-28 23:57:34 +0530

    face padding settings for lipsync

--- pages/Lipsync.py
+++ pages/Lipsync.py
@@ -18,6 +18,11 @@ class LipsyncPage(DarsAiPage):
         input_face: str
         input_audio: str
 
+        face_padding_top: int
+        face_padding_bottom: int
+        face_padding_left: int
+        face_padding_right: int
+
     class ResponseModel(BaseModel):
         output_video: str
 
@@ -25,16 +30,18 @@ class LipsyncPage(DarsAiPage):
         with st.form("my_form"):
             st.write(
                 """
-                ### Input Face
-                Upload a video/image that contains faces to use
+                #### Input Face
+                Upload a video/image that contains faces to use  
+                *Recommended - .mp4 / .png / .jpg* 
                 """
             )
             face_file = st.file_uploader("input face", label_visibility="collapsed")
 
             st.write(
                 """
-                ### Input Audio
-                Upload the video/audio file to use as audio source for lip syncing
+                #### Input Audio
+                Upload the video/audio file to use as audio source for lipsyncing  
+                *Recommended - .wav / .mp3*
                 """
             )
             audio_file = st.file_uploader("input audio", label_visibility="collapsed")
@@ -55,6 +62,45 @@ class LipsyncPage(DarsAiPage):
 
         return submitted
 
+    def render_settings(self):
+        st.write(
+            """
+        ### Face Padding
+        Adjust the detected face bounding box. Often leads to improved results.  
+        Recommended to give atleast 10 padding for the chin region. 
+        """
+        )
+
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            st.slider(
+                "Head",
+                min_value=0,
+                max_value=100,
+                key="face_padding_top",
+            )
+        with col2:
+            st.slider(
+                "Chin",
+                min_value=0,
+                max_value=100,
+                key="face_padding_bottom",
+            )
+        with col3:
+            st.slider(
+                "Left Cheek",
+                min_value=0,
+                max_value=100,
+                key="face_padding_left",
+            )
+        with col4:
+            st.slider(
+                "Right Cheek",
+                min_value=0,
+                max_value=100,
+                key="face_padding_right",
+            )
+
     def run(self, state: dict) -> typing.Iterator[str | None]:
         request = self.RequestModel.parse_obj(state)
 
@@ -63,7 +109,11 @@ class LipsyncPage(DarsAiPage):
         model = replicate.models.get("devxpy/cog-wav2lip").versions.get(
             "37084febcf7a530c994fc607937ada9101975eba360f547b1aa1e020bf8f1317"
         )
-        output = model.predict(face=request.input_face, audio=request.input_audio)
+        output = model.predict(
+            face=request.input_face,
+            audio=request.input_audio,
+            pads=f"{request.face_padding_top} {request.face_padding_bottom} {request.face_padding_left} {request.face_padding_right}",
+        )
 
         state["output_video"] = upload_file_from_bytes(
             "output.mp4", requests.get(output).content
@@ -73,24 +123,27 @@ class LipsyncPage(DarsAiPage):
         col1, col2 = st.columns(2)
 
         with col1:
-            st.write("Input Face")
             input_face = st.session_state.get("input_face")
-            if input_face:
+            if not input_face:
+                st.empty()
+            elif input_face.endswith(".mp4"):
+                st.write("Input Face (Video)")
                 st.video(input_face)
             else:
-                st.empty()
+                st.write("Input Face (Image)")
+                st.image(input_face)
 
-            st.write("Input Audio")
             input_audio = st.session_state.get("input_audio")
             if input_audio:
+                st.write("Input Audio")
                 st.audio(input_audio)
             else:
                 st.empty()
 
         with col2:
-            st.write("Output Video")
             output_video = st.session_state.get("output_video")
             if output_video:
+                st.write("Output Video")
                 st.video(output_video)
             else:
                 st.empty()
