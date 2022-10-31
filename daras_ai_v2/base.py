23aa551e3c58991a52f74dcced01d7994d5cf954
Sean Blagsvedt <sean@blagsvedt.com>
2022-10-30 18:25:47 -0700

    Add Gooey logo and terms

--- daras_ai_v2/base.py
+++ daras_ai_v2/base.py
@@ -227,9 +227,12 @@ def logo():
     st.markdown(
         f"""
         <a href="/" target="_self">
-            <img width=150 src="https://storage.googleapis.com/dara-c1b52.appspot.com/daras_ai/media/logo.webp"></img>
+            <img width=150 src="https://storage.googleapis.com/dara-c1b52.appspot.com/gooey/gooey-logo-rect.png"></img>
         </a>
-        <a style="position: absolute; right: 0px" href="https://dara.network/privacy/">Privacy Policy</a>
+        <span style="position: absolute; right: 0px">
+            <a href="https://dara.network/privacy/">Privacy</a> & 
+            <a href="https://dara.network/terms/">Terms</a>
+        </span>
         """,
         unsafe_allow_html=True,
     )
