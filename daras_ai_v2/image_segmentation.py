fd84d5ea0af098ae2229d3e8aeb2ba47aba8773f
Dev Aggarwal <devxpy@gmail.com>
2022-11-08 00:49:40 +0530

    use correct endpoint for dichotomous_image_segmentation

--- daras_ai_v2/image_segmentation.py
+++ daras_ai_v2/image_segmentation.py
@@ -1,9 +1,9 @@
-from daras_ai_v2.gpu_server import call_gpu_server_b64
+from daras_ai_v2.gpu_server import call_gpu_server_b64, GpuEndpoints
 
 
 def dichotomous_image_segmentation(input_image: str) -> bytes:
     return call_gpu_server_b64(
-        port=5004,
+        endpoint=GpuEndpoints.dichotomous_image_segmentation,
         input_data={
             "input_image": input_image,
         },
