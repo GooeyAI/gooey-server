7dd0f2b8090f12a5e89d0cc2f10fcc3186aad1ff
singhmitesh <singhmitesh19@gmail.com>
2022-11-09 23:22:04 +0530

    Add description LipsyncTTS

--- pages/LipsyncTTS.py
+++ pages/LipsyncTTS.py
@@ -78,6 +78,19 @@ class LipsyncTTSPage(LipsyncPage, TextToSpeechPage):
 
         return submitted
 
+
+    def render_description(self):
+        st.write(
+            """
+                *Converts text to audio and Lipsyncs with video*
+
+                How It Works:
+
+                1. Takes Text input + Face input
+                2. Generates audio file in voice of your choice 
+                3. Merges the audio and video.
+            """
+        )
     def render_settings(self):
         LipsyncPage.render_settings(self)
         TextToSpeechPage.render_settings(self)
