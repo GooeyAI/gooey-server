96de061b65b5521d16cfb88da40a0eab4163c678
Dev Aggarwal <devxpy@gmail.com>
2022-11-16 12:23:55 +0530

    separate ports for api & streamlit

--- scripts/run-prod.sh
+++ scripts/run-prod.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [ "$STREAMLIT_SERVER" ]; then
-  streamlit run Home.py --server.address=0.0.0.0 --server.port=8000
+  streamlit run Home.py --server.address=0.0.0.0 --server.port=8001
 fi
 
 if [ "$API_SERVER" ]; then
-  uvicorn server:app --host 0.0.0.0 --port 8000
+  uvicorn server:app --host 0.0.0.0 --port 8002
 fi
