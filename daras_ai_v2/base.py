3ae1a050f3cbdc8450a2870655d06522ae6b8893
Sean Blagsvedt <sean@blagsvedt.com>
2022-11-15 22:11:10 -0800

    Updated Logo and Colors

--- daras_ai_v2/base.py
+++ daras_ai_v2/base.py
@@ -228,7 +228,7 @@ def logo():
     st.markdown(
         f"""
         <a href="/" target="_self">
-            <img width=150 src="https://storage.googleapis.com/dara-c1b52.appspot.com/gooey/gooey-logo-rect.png"></img>
+            <img style="width:150px; height:71px" src="https://storage.googleapis.com/dara-c1b52.appspot.com/gooey/gooey_logo_300x142.png"></img>
         </a>
         <span style="position: absolute; right: 0px">
             <a href="https://dara.network/privacy/">Privacy</a> & 
