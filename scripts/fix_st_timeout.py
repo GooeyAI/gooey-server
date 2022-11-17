e4a9dcfc1c4a17f103b0c1f194209de73ab130e8
Dev Aggarwal <devxpy@gmail.com>
2022-11-18 00:40:08 +0530

    st timeout hack??

--- scripts/fix_st_timeout.py
+++ scripts/fix_st_timeout.py
@@ -1,12 +1,18 @@
 import site
 from pathlib import Path
 
-idx = 115567
 
 site_packages = Path(site.getsitepackages()[0])
 js_file = site_packages / f"streamlit/static/static/js/main.6569bfa5.chunk.js"
 
 txt = js_file.read_text()
+
+# Timeout fired after cancellation
+idx = 115567
 txt = txt[:idx] + "1e4" + txt[idx + 3 :]
+
+# ,500
+idx = 113814
+txt = txt[:idx] + "1000" + txt[idx + 3 :]
 
 js_file.write_text(txt)
