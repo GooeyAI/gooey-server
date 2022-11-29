6bc45f60902ee1d2459c418918225c1be3e43974
Dev Aggarwal <devxpy@gmail.com>
2022-11-29 15:19:25 +0530

    rollback

--- scripts/run-prod.sh
+++ scripts/run-prod.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-streamlit run Home.py --server.address=0.0.0.0 --server.port=8501 &
+if [ "$STREAMLIT_SERVER" ]; then
+  streamlit run Home.py --server.address=0.0.0.0 --server.port=8000
+fi
 
-uvicorn server:app --host 0.0.0.0 --port 8000 &
-
-wait
+if [ "$API_SERVER" ]; then
+  uvicorn server:app --host 0.0.0.0 --port 8000
+fi
