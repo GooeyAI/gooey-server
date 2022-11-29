235a72bf89a2a7d0b7b76470b7a6171aa94df899
Dev Aggarwal <devxpy@gmail.com>
2022-11-29 15:16:36 +0530

    run uvicorn and streamlit in same container

--- scripts/run-prod.sh
+++ scripts/run-prod.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-streamlit run Home.py --server.address=0.0.0.0 --server.port=8501
+streamlit run Home.py --server.address=0.0.0.0 --server.port=8501 &
 
-uvicorn server:app --host 0.0.0.0 --port 8000
+uvicorn server:app --host 0.0.0.0 --port 8000 &
+
+wait
