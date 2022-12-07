720aa81cb9281232ef3fb2451b81dee2a0c28170
singhmitesh <singhmitesh19@gmail.com>
2022-12-07 14:22:37 +0530

    use env for admin emails

--- daras_ai_v2/settings.py
+++ daras_ai_v2/settings.py
@@ -1,7 +1,7 @@
 import os
 
 import firebase_admin
-from decouple import config, UndefinedValueError
+from decouple import config, UndefinedValueError, Csv
 from google.oauth2 import service_account
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -52,10 +52,4 @@ SCALESERP_API_KEY = config("SCALESERP_API_KEY", None)
 # timeout for fetching external urls in the wild
 EXTERNAL_REQUEST_TIMEOUT_SEC = config("EXTERNAL_REQUEST_TIMEOUT_SEC", 10)
 
-ADMIN_EMAILS = [
-    "archana@dara.network",
-    "dev@dara.network",
-    "sean@dara.network",
-    "shreeparna@dara.network",
-    "mitesh@dara.network",
-]
+ADMIN_EMAILS = config("ADMIN_EMAILS", cast=Csv())
