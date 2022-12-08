75ad020d26feff6dcb8eba5927489e1e2e522be0
singhmitesh <singhmitesh19@gmail.com>
2022-12-08 18:51:04 +0530

    black formatting

--- daras_ai_v2/base.py
+++ daras_ai_v2/base.py
@@ -276,11 +276,11 @@ class BasePage:
             doc = snapshot.to_dict()
 
             url = (
-                    furl(
-                        settings.APP_BASE_URL,
-                        query_params={"example_id": example_id},
-                    )
-                    / self.slug
+                furl(
+                    settings.APP_BASE_URL,
+                    query_params={"example_id": example_id},
+                )
+                / self.slug
             ).url
 
             col1, col2, col3, *_ = st.columns(6)
@@ -394,8 +394,8 @@ class BasePage:
 
 
 def set_saved_doc(
-        doc_ref: firestore.DocumentReference,
-        updated_state: dict,
+    doc_ref: firestore.DocumentReference,
+    updated_state: dict,
 ):
     doc_ref.set(updated_state)
     # saved_state = get_saved_doc(doc_ref)
@@ -425,10 +425,10 @@ def get_saved_doc_nocahe(doc_ref):
 
 # @cache_and_refresh
 def list_all_docs(
-        collection_id="daras-ai-v2",
-        *,
-        document_id: str = None,
-        sub_collection_id: str = None,
+    collection_id="daras-ai-v2",
+    *,
+    document_id: str = None,
+    sub_collection_id: str = None,
 ):
     db = firestore.Client()
     db_collection = db.collection(collection_id)
@@ -439,11 +439,11 @@ def list_all_docs(
 
 
 def get_doc_ref(
-        document_id: str,
-        *,
-        collection_id="daras-ai-v2",
-        sub_collection_id: str = None,
-        sub_document_id: str = None,
+    document_id: str,
+    *,
+    collection_id="daras-ai-v2",
+    sub_collection_id: str = None,
+    sub_document_id: str = None,
 ):
     db = firestore.Client()
     db_collection = db.collection(collection_id)
@@ -493,7 +493,7 @@ curl -X 'POST' \
 
 
 def get_example_request_body(
-        request_model: typing.Type[BaseModel], state: dict
+    request_model: typing.Type[BaseModel], state: dict
 ) -> dict:
     return {
         field_name: state.get(field_name)
