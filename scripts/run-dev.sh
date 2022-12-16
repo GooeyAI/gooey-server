89c85e214c530138475827bf6f9b6fae29a0ed02
singhmitesh <singhmitesh19@gmail.com>
2022-12-16 19:15:49 +0530

    update sentry env

--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -1,4 +1,4 @@
-SENTRY_ENVIRONMENT="streamlit" streamlit run Home.py --server.headless true --server.port 8501 &
-SENTRY_ENVIRONMENT="fast-api" uvicorn server:app --reload --port 8000 &
+(export SENTRY_ENVIRONMENT="streamlit" && streamlit run Home.py --server.headless true --server.port 8501 &)
+(export SENTRY_ENVIRONMENT="fast-api" && uvicorn server:app --reload --port 8000 &)
 nginx -c $PWD/nginx.dev.conf &
 wait
