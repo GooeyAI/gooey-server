50457b141b6220c598deab9caee6ba47949e4251
singhmitesh <singhmitesh19@gmail.com>
2023-01-31 16:54:31 +0530

    move cutout_img first for correct image in ImageSegmentation.py

--- daras_ai_v2/base.py
+++ daras_ai_v2/base.py
@@ -914,8 +914,8 @@ class BasePage:
     def preview_image(self, state: dict) -> str | None:
         out = (
             state.get("output_images")
-            or state.get("output_image")
             or state.get("cutout_image")
+            or state.get("output_image")
         )
         return extract_nested_str(out) or DEFAULT_META_IMG
 
