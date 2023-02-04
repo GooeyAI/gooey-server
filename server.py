a57b69171108269a0a58578bec1d2d2c636e9a1c
singhmitesh <singhmitesh19@gmail.com>
2023-02-04 13:53:06 +0530

    fix: wrong import

--- server.py
+++ server.py
@@ -39,7 +39,7 @@ from daras_ai_v2.meta_content import (
     meta_title_for_page,
     meta_description_for_page,
 )
-from daras_ai_v2.meta_preview_url import meta_preview_url, DEFAULT_META_IMG
+from daras_ai_v2.meta_preview_url import meta_preview_url
 from daras_ai_v2.settings import templates
 from gooey_token_authentication1.token_authentication import api_auth_header
 from recipes.ChyronPlant import ChyronPlantPage
