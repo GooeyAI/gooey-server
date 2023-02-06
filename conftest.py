08fe0a4325e663dcd1a6a93b963a95b160ccd219
Dev Aggarwal <devxpy@gmail.com>
2023-02-06 18:01:39 +0530

    ensure test runner user has miniumum credits

--- conftest.py
+++ conftest.py
@@ -2,9 +2,24 @@ import pytest
 from decouple import config
 from firebase_admin import auth
 
+from daras_ai_v2 import db
 from gooey_token_authentication1.token_authentication import authenticate_credentials
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def test_auth_user() -> auth.UserRecord:
-    return authenticate_credentials(config("TEST_RUNNER_API_KEY"))
+    email = config("TEST_RUNNER_EMAIL")
+    api_key = config("TEST_RUNNER_API_KEY")
+
+    # get test runner user
+    try:
+        user = auth.get_user_by_email(email)
+    except auth.UserNotFoundError:
+        user = auth.create_user(email=email)
+
+    # ensure user has credits
+    doc_ref = db.get_user_doc_ref(user.uid)
+    doc_ref.set({db.USER_BALANCE_FIELD: 1000}, merge=True)
+
+    # authenticate via api key
+    return authenticate_credentials(api_key)
