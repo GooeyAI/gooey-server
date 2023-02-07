d6340478d44f6f414647d72a197537b3720a6306
singhmitesh <singhmitesh19@gmail.com>
2023-02-07 14:25:13 +0530

    Add tests to ping all public endpoints and assert response.ok

---
+++ tests/test_public_endpoints.py
@@ -0,0 +1,22 @@
+from furl import furl
+from starlette.routing import Route
+from starlette.testclient import TestClient
+
+from daras_ai_v2.settings import APP_BASE_URL
+from server import app
+
+client = TestClient(app)
+excluded_routes = [
+    "/__/fb/webhook/",
+]
+
+
+def test_apis_basic():
+    for route in app.routes:
+        if isinstance(route, Route):
+            get_method_allowed = "GET" in route.methods
+            not_in_excluded_routes = route.path not in excluded_routes
+            if get_method_allowed and not_in_excluded_routes:
+                url = furl(APP_BASE_URL).add(path=route.path).url
+                r = client.get(url)
+                assert r.ok, r.content
