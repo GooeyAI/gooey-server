c2913ce1cb78018107ae0f5fcc3f152ec72048aa
Dev Aggarwal <devxpy@gmail.com>
2023-02-22 02:07:38 +0530

    lower sentry traces_sample_rate

--- daras_ai_v2/settings.py
+++ daras_ai_v2/settings.py
@@ -20,7 +20,7 @@ if not DEBUG:
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production.
-        traces_sample_rate=1.0,
+        traces_sample_rate=0.1,
         send_default_pii=True,
         integrations=[
             ThreadingIntegration(propagate_hub=True),
