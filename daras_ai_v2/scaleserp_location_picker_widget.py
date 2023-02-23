b232b6a724fc5e15e100f08673b6ebcb6f4c522b
singhmitesh <singhmitesh19@gmail.com>
2023-02-23 23:46:21 +0530

    set multiselect value via defaults

--- daras_ai_v2/scaleserp_location_picker_widget.py
+++ daras_ai_v2/scaleserp_location_picker_widget.py
@@ -5,11 +5,9 @@ from recipes.scale_serp import scaleserp_locations_list
 
 def scaleserp_location_picker():
     defaults = st.session_state.get("scaleserp_locations", [])
-    st.multiselect(
+    selected_countries = st.multiselect(
         "**ScaleSERP [Location](https://www.scaleserp.com/docs/search-api/reference/google-countries)**",
         options=scaleserp_locations_list,
         default=defaults,
-        key="scaleserp_locations",
     )
-
-
+    st.session_state["scaleserp_locations"] = selected_countries
