e7b625d2d437e3580eab81e451f129ef32381a1f
singhmitesh <singhmitesh19@gmail.com>
2023-03-16 17:03:00 +0530

    update help text in CompareText2Img.py

--- recipes/CompareText2Img.py
+++ recipes/CompareText2Img.py
@@ -78,14 +78,25 @@ class CompareText2ImgPage(BasePage):
         st.text_area(
             """
             ### 👩‍💻 Prompt
-            Describe the scene that you'd like to generate. 
+            Describe the scene that you'd like to generate.
             """,
             key="text_prompt",
             placeholder="Iron man",
         )
+        st.caption(
+            """
+            Refer to the saved examples or our basic prompt guide in the ‘Details’ dropdown menu.
+        """
+        )
         st.write("#### 🧨 Compare Image Models")
         st.caption(
             "Each selected model costs 2 credits to run except for Dall-E which is 15 credits per rendered image."
+        )
+        st.caption(
+            """
+        Confused about what each model looks like?
+        [Check out our prompt guide](https://docs.google.com/presentation/d/1RaoMP0l7FnBZovDAR42zVmrUND9W5DW6eWet-pi6kiE/edit#slide=id.g210b1678eba_0_26).
+        """
         )
         enum_multiselect(
             Text2ImgModels,
@@ -97,15 +108,57 @@ class CompareText2ImgPage(BasePage):
         assert st.session_state["selected_models"], "Please select at least one model"
 
     def render_description(self):
-        st.write(
+        st.markdown(
             """
-            This recipe takes any text and renders an image using multiple Text2Image engines. 
-            
-            Use it to understand which image generator e.g. DallE or Stable Diffusion is best for your particular prompt. 
+            This recipe takes any text and renders an image using multiple Text2Image engines.
+            Use it to understand which image generator e.g. DallE or Stable Diffusion is best for your particular prompt.
+
+            #### Prompting 101: 
+
+            ##### Step 1: Create an idea or visualization in your mind. 
+            `I want an image of an astronaut in a space suit walking on the streets of Mumbai.`
+            """
+        )
+        st.markdown(
+            """
+            ##### Step 2: Think about your descriptors and break it down as follows: 
+
+            - What is the Medium of this image? \\
+            eg. It is a painting, a sculpture, an old photograph, portrait, 3D render, etc.\n
+            - What/Who are the Subject(s) or Main Object(s) in the image? \\
+            eg. A human, an animal, an identity like gender, race, or occupation like dancer, astronaut etc. \n
+            - What is the Style? \\
+            eg. Is it Analogue photography, a watercolor, a line drawing, digital painting etc. \n
+            - What are the Details? \\
+            eg. facial features or expressions, the space and landscape, lighting or the colours etc. 
+            """
+        )
+        st.markdown(
+            """
+            ##### Step 3: Construct your prompt:
+            `An analogue film still of an astronaut in a space suit walking on the busy streets of Mumbai, golden light on the astronaut, 4k`
+            """
+        )
+        st.markdown(
+            """
+            You can keep editing your prompt until you have your desired output. Consider AI generators as a collaborative tool. 
+            #### What is the difference between Submit and Regenerate? 
+            Each AI generation has a unique Seed number. A random seed is created when you initiate the first run on clicking the Submit button. The seed is maintained as you continue editing the image with different setting options on each subsequent Submit click.\n
+            However, by clicking the Regenerate button, the AI will generate a new Seed and a completely new/different set of outputs.
             """
         )
 
     def render_settings(self):
+        st.write(
+            """
+            Customize the image output for your text prompt with these Settings. 
+            """
+        )
+        st.caption(
+            """
+            You can also enable ‘Edit Instructions’ to use InstructPix2Pix that allows you to change your generated image output with a follow-up written instruction.
+            """
+        )
         if st.checkbox("📝 Edit Instructions"):
             st.text_area(
                 """
@@ -198,7 +251,7 @@ class CompareText2ImgPage(BasePage):
                 st.image(img, caption=Text2ImgModels[key].value)
 
     def preview_description(self, state: dict) -> str:
-        return "Create multiple AI photos from Stable Diffusion (1.5 -> 2.1, Open/Midjourney) DallE from one prompt. Determine which AI image generator from OpenAI, Stability.AI works best for your text."
+        return "Create multiple AI photos from one prompt using Stable Diffusion (1.5 -> 2.1, Open/Midjourney), DallE, and other models.  Find out which AI Image generator works best for your text prompt on comparing OpenAI, Stability.AI etc."
 
     def get_price(self) -> int:
         selected_models = st.session_state.get("selected_models", [])
