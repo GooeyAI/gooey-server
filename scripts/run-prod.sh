5f09a1a4afe579e1e7831cfa68bcc0cb5f8d63da
Dev Aggarwal <devxpy@gmail.com>
2023-05-04 02:58:39 +0530

    deploy on uvicorn only

--- scripts/run-prod.sh
+++ scripts/run-prod.sh
@@ -5,7 +5,5 @@ if [ "$RUN_DJANGO" ]; then
   ./manage.py collectstatic
   SENTRY_ENVIRONMENT="django" gunicorn gooeysite.wsgi --bind 0.0.0.0:8000 --threads $WEB_CONCURRENCY
 else
-  SENTRY_ENVIRONMENT="streamlit" streamlit run Home.py --server.address=0.0.0.0 --server.port=8501 &
-  SENTRY_ENVIRONMENT="fast-api" uvicorn server:app --host 0.0.0.0 --workers $WEB_CONCURRENCY --port 8000 &
-  wait
+  SENTRY_ENVIRONMENT="fast-api" uvicorn server:app --host 0.0.0.0 --workers $WEB_CONCURRENCY --port 8000
 fi
