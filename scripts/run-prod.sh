4b25d56084a93159c1947635e204cf47469c821e
Dev Aggarwal <devxpy@gmail.com>
2023-05-04 02:58:39 +0530

    deploy on uvicorn only

--- scripts/run-prod.sh
+++ scripts/run-prod.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-SENTRY_ENVIRONMENT="streamlit" streamlit run Home.py --server.address=0.0.0.0 --server.port=8501 &
-SENTRY_ENVIRONMENT="fast-api" uvicorn server:app --host 0.0.0.0 --port 8000 &
-wait
+uvicorn server:app --host 0.0.0.0 --port 8000
