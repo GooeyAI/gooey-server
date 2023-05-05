13a17a1bc13e39322a9dbdb285cc3a89be038c0c
Dev Aggarwal <devxpy@gmail.com>
2023-05-06 03:56:06 +0530

    set django allowed hosts

--- daras_ai_v2/settings.py
+++ daras_ai_v2/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = "*"
+INTERNAL_IPS = ["127.0.0.1"]
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 
 # Application definition
 
