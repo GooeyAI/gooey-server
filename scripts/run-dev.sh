e7ce0edbab8066739da9028f69bbfd06def229a2
Dev Aggarwal <devxpy@gmail.com>
2023-05-05 11:52:36 +0530

    kill streamlit

--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -1,8 +1,3 @@
-streamlit run Home.py \
-  --client.showErrorDetails true \
-  --server.enableXsrfProtection false \
-  --server.headless true \
-  --server.port 8501
 python scripts/uvicorn_run_dev.py
 nginx -c $PWD/nginx.dev.conf &
 wait
