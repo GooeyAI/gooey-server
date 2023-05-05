c073efcb08de57d6cbad0a6a9529db990eb33bd8
Dev Aggarwal <devxpy@gmail.com>
2023-05-06 03:20:23 +0530

    fix gunicorn args

--- scripts/run-prod.sh
+++ scripts/run-prod.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 if [ "$RUN_DJANGO" ]; then
-  SENTRY_ENVIRONMENT="django" gunicorn gooeysite.wsgi --bind 0.0.0.0 --threads $WEB_CONCURRENCY --port 8000
+  SENTRY_ENVIRONMENT="django" gunicorn gooeysite.wsgi --bind 0.0.0.0:8000 --threads $WEB_CONCURRENCY
 else
   SENTRY_ENVIRONMENT="streamlit" streamlit run Home.py --server.address=0.0.0.0 --server.port=8501 &
   SENTRY_ENVIRONMENT="fast-api" uvicorn server:app --host 0.0.0.0 --workers $WEB_CONCURRENCY --port 8000 &
