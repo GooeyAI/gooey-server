c09f422a034097c5d643d397dd0becb5dd43c019
Dev Aggarwal <devxpy@gmail.com>
2023-05-06 03:10:00 +0530

    export requirements

--- daras_ai_v2/settings.py
+++ daras_ai_v2/settings.py
@@ -80,13 +80,29 @@ WSGI_APPLICATION = "gooeysite.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+try:
+    PGHOST = config("PGHOST")
+    PGPORT = config("PGPORT")
+    PGDATABASE = config("PGDATABASE")
+    PGUSER = config("PGUSER")
+    PGPASSWORD = config("PGPASSWORD")
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": PGDATABASE,
+            "USER": PGUSER,
+            "PASSWORD": PGPASSWORD,
+            "HOST": PGHOST,
+            "PORT": PGPORT,
+        }
     }
-}
+except UndefinedValueError:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
