f767c52d0e16c2d8a567035ef17dfe6757948366
Dev Aggarwal <devxpy@gmail.com>
2023-06-13 22:39:06 +0530

    consistent prod port

--- scripts/run-prod.sh
+++ scripts/run-prod.sh
@@ -13,7 +13,7 @@ if [ "$RUN_DJANGO" ]; then
   ./manage.py collectstatic
   SENTRY_ENVIRONMENT="django" gunicorn gooeysite.wsgi --bind 0.0.0.0:8000 --threads "${WEB_CONCURRENCY:-1}"
 elif [ "$RUN_STREAMLIT" ]; then
-  SENTRY_ENVIRONMENT="streamlit" streamlit run Home.py --server.address=0.0.0.0 --server.port=8501
+  SENTRY_ENVIRONMENT="streamlit" streamlit run Home.py --server.address=0.0.0.0 --server.port=8000
 else
   SENTRY_ENVIRONMENT="fastapi" uvicorn server:app --host 0.0.0.0 --workers "${WEB_CONCURRENCY:-1}" --port 8000
 fi
