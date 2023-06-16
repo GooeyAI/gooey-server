b34ff44aeaff016a8e15227e0e851ab6512eae0a
Dev Aggarwal <devxpy@gmail.com>
2023-06-17 02:25:18 +0530

    run jupyter lab

--- scripts/run-prod.sh
+++ scripts/run-prod.sh
@@ -8,7 +8,10 @@ if [ "$DUMP_DATABASE" ] && [ "$PGDATABASE" ]; then
   pg_dump $DUMP_DATABASE | psql -q $PGDATABASE
 fi
 
-if [ "$RUN_DJANGO" ]; then
+if [ "$RUN_JUPYTER" ]; then
+  pip install jupyterlab
+  jupyter lab --ip 0.0.0.0 --port 8000 --allow-root
+elif [ "$RUN_DJANGO" ]; then
   ./manage.py migrate
   ./manage.py collectstatic
   SENTRY_ENVIRONMENT="django" gunicorn gooeysite.wsgi --bind 0.0.0.0:8000 --threads "${WEB_CONCURRENCY:-1}"
