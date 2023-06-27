23418bc454ba0af091a093cec9c204b74d6fbbd5
Dev Aggarwal <devxpy@gmail.com>
2023-06-27 13:50:39 +0530

    more detailed datetime format

--- daras_ai_v2/settings.py
+++ daras_ai_v2/settings.py
@@ -145,6 +145,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+DATETIME_FORMAT = "N j, D, Y, h:i:s A"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
