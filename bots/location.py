c414fec6b5138a53a9153e4c0d13f40af508d958
Eli Weise <eli@dara.network>
2023-07-24 14:01:34 -0700

    Add extra columns to spreadsheet
    Add formatted address and latitude and longitude columns to google spreadsheet because those are the fields that should be saved

--- bots/location.py
+++ bots/location.py
@@ -11,14 +11,15 @@ df = pd.DataFrame()
 
 gmaps = googlemaps.Client(key='AIzaSyC7j8WI-P_BZQogR809B2QbaH_aP1KsVeM')
 
-location: str = "Chicago Bean"
+location: str = "busia"
 
 geocode_result = gmaps.geocode(location)
 
 # Create a column
 df["location"] = [location]
 df["JSON"] = [geocode_result]
-
+df["formatted address"] = [geocode_result[0]["formatted_address"]]
+df["latitude and longitude"] = [geocode_result[0]["geometry"]["location"]]
 
 #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
 sh = gc.open("saved locations")
@@ -26,5 +27,5 @@ sh = gc.open("saved locations")
 #select the first sheet 
 wks = sh[0]
 
-#update the first sheet with df, starting at cell B2. 
+#update the first sheet with df, starting at cell A1. 
 wks.set_dataframe(df,(1,1))
