4afc281db31a8a2b6b6f696d2ed365122f24687c
Dev Aggarwal <devxpy@gmail.com>
2023-08-01 14:17:34 +0530

    update dashboard urls

--- Home.py
+++ Home.py
@@ -5,10 +5,10 @@ assert wsgi
 """
 ## Welcome to Gooey.AI Dashboard
 
-Name|Link
----|---
-Main App | [gooey.ai](https://gooey.ai/explore)
-Admin | [admin.gooey.ai](https://admin.gooey.ai)
-Test App | [gooey-gui.us-1.gooey.ai](https://gooey-gui.us-1.gooey.ai/explore)
-Test Admin | [gooey-admin-test.us-1.gooey.ai](http://gooey-admin-test.us-1.gooey.ai)
+_|Prod|Test
+---|---|---
+Home | [gooey.ai](https://gooey.ai) | [gooey-gui-test.us-1.gooey.ai](https://gooey-gui-test.us-1.gooey.ai)
+App | [gooey.ai/explore](https://gooey.ai/explore) | [gooey-gui-test.us-1.gooey.ai/explore](https://gooey-gui-test.us-1.gooey.ai/explore)
+Admin | [admin.gooey.ai](https://admin.gooey.ai) | [gooey-admin-test.us-1.gooey.ai](https://gooey-admin-test.us-1.gooey.ai/)
+Dash | [dash.gooey.ai](https://dash.gooey.ai) | [gooey-dash-test.us-1.gooey.ai](https://gooey-dash-test.us-1.gooey.ai/)
 """
