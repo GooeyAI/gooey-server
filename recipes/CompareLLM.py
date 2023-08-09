08eaea80f0cf5d9f37a6867e0d0b12ae244099d0
Alexander Metzger <alexander@metzger.dk>
2023-08-08 21:02:01 -0700

    text-bison (PaLM 2 Text) support

--- recipes/CompareLLM.py
+++ recipes/CompareLLM.py
@@ -149,11 +149,9 @@ class CompareLLMPage(BasePage):
                 case LargeLanguageModels.text_ada_001.name:
                     total += 1
                 case LargeLanguageModels.palm2_text.name:
-                    total += 15
+                    total += 300
                 case LargeLanguageModels.palm2_chat.name:
-                    total += 10
-                case LargeLanguageModels.llama2_70b_chat.name:
-                    total += 5
+                    total += 150
         return total * state.get("num_outputs", 1)
 
     def related_workflows(self) -> list:
