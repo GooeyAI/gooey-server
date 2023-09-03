e34dadc3e5c78dd56b0d85d9c2eba4491fb45417
Dev Aggarwal <devxpy@gmail.com>
2023-09-04 04:58:28 +0530

    migrate lipsync to k8s

--- daras_ai_v2/lipsync_api.py
+++ daras_ai_v2/lipsync_api.py
@@ -1,15 +1,20 @@
-from daras_ai_v2.gpu_server import call_gpu_server_b64, GpuEndpoints
+from daras_ai_v2.gpu_server import call_celery_task_outfile
 
 
 def wav2lip(*, face: str, audio: str, pads: (int, int, int, int)) -> bytes:
-    return call_gpu_server_b64(
-        endpoint=GpuEndpoints.wav2lip,
-        input_data={
-            "face": face,
-            "audio": audio,
-            "pads": " ".join(map(str, pads)),
+    return call_celery_task_outfile(
+        "wav2lip",
+        pipeline=dict(
+            model_id="wav2lip_gan.pth",
+        ),
+        inputs=dict(
+            face=face,
+            audio=audio,
+            pads=pads,
             # "out_height": 480,
             # "smooth": True,
             # "fps": 25,
-        },
+        ),
+        content_type="video/mp4",
+        filename=f"gooey.ai lipsync.mp4",
     )[0]
