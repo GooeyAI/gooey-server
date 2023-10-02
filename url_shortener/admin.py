12dcd03e2b30852b154104bbcf6d5335f4db921d
Alexander Metzger <alexander@metzger.dk>
2023-10-02 02:10:14 -0700

    added click analytics models, logic, and admin

--- url_shortener/admin.py
+++ url_shortener/admin.py
@@ -28,6 +28,7 @@ class ShortenedURLAdmin(admin.ModelAdmin):
         "disabled",
         "created_at",
         "updated_at",
+        "use_analytics",
     ]
     readonly_fields = [
         "clicks",
@@ -35,6 +36,7 @@ class ShortenedURLAdmin(admin.ModelAdmin):
         "updated_at",
         "shortened_url",
         "get_saved_runs",
+        "get_click_analytics",
     ]
     exclude = ["saved_runs"]
     ordering = ["created_at"]
@@ -48,3 +50,61 @@ class ShortenedURLAdmin(admin.ModelAdmin):
     @admin.display(description="Saved Runs")
     def get_saved_runs(self, obj: models.ShortenedURL):
         return list_related_html_url(obj.saved_runs, show_add=False)
+
+    @admin.display(description="Analytic Clicks")
+    def get_click_analytics(self, obj: models.ShortenedURL):
+        if not obj.use_analytics:
+            return []
+        return list_related_html_url(
+            models.ClickAnalytic.objects.filter(shortened_url__pk=obj.pk),
+            query_param="shortened_url__id__exact",
+            instance_id=obj.pk,
+            show_add=False,
+        )
+
+
+@admin.register(models.ClickAnalytic)
+class ClickAnalyticAdmin(admin.ModelAdmin):
+    list_filter = [
+        "shortened_url__url",
+        "created_at",
+    ]
+    search_fields = (
+        ["ip_address"]
+        + [
+            f"shortened_url__saved_runs__{field}"
+            for field in SavedRunAdmin.search_fields
+        ]
+        + [f"shortened_url__user__{field}" for field in AppUserAdmin.search_fields]
+        + [f"shortened_url__{field}" for field in ShortenedURLAdmin.search_fields]
+    )
+    list_display = [
+        "ip_address",
+        "platform",
+        "operating_system",
+        "device_model",
+        "country_name",
+        "city_name",
+        "created_at",
+        "location_data",
+        "user_agent",
+        "get_saved_runs",
+    ]
+    readonly_fields = [
+        "ip_address",
+        "platform",
+        "operating_system",
+        "device_model",
+        "country_name",
+        "city_name",
+        "created_at",
+        "location_data",
+        "user_agent",
+    ]
+    exclude = ["saved_runs"]
+    ordering = ["created_at"]
+    actions = [export_to_csv, export_to_excel]
+
+    @admin.display(description="Saved Runs")
+    def get_saved_runs(self, obj: models.ClickAnalytic):
+        return list_related_html_url(obj.shortened_url.saved_runs, show_add=False)
--- url_shortener/routers.py
+++ url_shortener/routers.py
@@ -1,16 +1,18 @@
 from django.db.models import F
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import RedirectResponse
 from fastapi.responses import Response
+import re
+import requests
 
-from url_shortener.models import ShortenedURL
+from url_shortener.models import ShortenedURL, ClickAnalytic
 
 app = APIRouter()
 
 
 @app.api_route("/2/{hashid}", methods=["GET", "POST"])
 @app.api_route("/2/{hashid}/", methods=["GET", "POST"])
-def url_shortener(hashid: str):
+def url_shortener(hashid: str, request: Request):
     try:
         surl = ShortenedURL.objects.get_by_hashid(hashid)
     except ShortenedURL.DoesNotExist:
@@ -20,6 +22,68 @@ def url_shortener(hashid: str):
         return Response(status_code=410, content="This link has expired")
     # increment the click count
     ShortenedURL.objects.filter(id=surl.id).update(clicks=F("clicks") + 1)
+    if surl.use_analytics:
+        ip_address = request.client.host  # does not work for localhost or with nginx
+        user_agent = request.headers.get(
+            "user-agent", ""
+        )  # note all user agent info can be spoofed
+        platform = getPlatform(user_agent)
+        operating_system = getOperatingSystem(user_agent)
+        device_model = getAndroidDeviceModel(user_agent)
+        res = requests.get(f"https://iplist.cc/api/{ip_address}")
+        location_data = res.json() if res.ok else {}
+        # not all location data will always be available
+        country_name = location_data.get("countryname", "")
+        city_name = location_data.get("city", "")
+        ClickAnalytic.objects.create(
+            shortened_url=surl,
+            ip_address=ip_address,
+            user_agent=user_agent,
+            platform=platform,
+            operating_system=operating_system,
+            device_model=device_model,
+            location_data=location_data,
+            country_name=country_name,
+            city_name=city_name,
+        )
     return RedirectResponse(
         url=surl.url, status_code=303  # because youtu.be redirects are 303
     )
+
+
+def getPlatform(user_agent: str):
+    devices = [
+        "Android",
+        "webOS",
+        "iPhone",
+        "iPad",
+        "iPod",
+        "BlackBerry",
+        "IEMobile",
+        "Opera Mini",
+    ]
+    return "mobile" if any(device in user_agent for device in devices) else "desktop"
+
+
+def getOperatingSystem(user_agent: str):
+    if "Win" in user_agent:
+        return "Windows"
+    elif "Mac" in user_agent:
+        return "MacOS"
+    elif "Linux" in user_agent:
+        return "Linux"
+    elif "Android" in user_agent:
+        return "Android"
+    elif "like Mac" in user_agent:
+        return "iOS"
+    else:
+        return "Other"
+
+
+def getAndroidDeviceModel(user_agent: str):
+    regex = r"Android (\d+(?:\.\d+)*);"
+    matches = re.search(regex, user_agent)
+    if matches:
+        return matches.group(1)
+    else:
+        return "Other"
