e266df838a96746e16947cc6f4c9ab184fffaa98
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2023-10-11 03:57:38 +0530

    img2img: Check if image is NSFW before running the workflow

---
+++ daras_ai_v2/azure_image_moderation.py
@@ -0,0 +1,39 @@
+from typing import Any
+
+from furl import furl
+import requests
+
+from daras_ai_v2 import settings
+
+
+def get_auth_headers():
+    return {"Ocp-Apim-Subscription-Key": settings.AZURE_IMAGE_MODERATION_KEY}
+
+
+def run_moderator(image_url: str, cache: bool) -> dict[str, Any]:
+    url = str(
+        furl(settings.AZURE_IMAGE_MODERATION_ENDPOINT)
+        / "contentmoderator/moderate/v1.0/ProcessImage/Evaluate"
+    )
+    r = requests.post(
+        url,
+        params={"CacheImage": f"{str(cache).lower()}"},
+        headers=get_auth_headers(),
+        json={"DataRepresentation": "URL", "Value": image_url},
+    )
+    r.raise_for_status()
+    return r.json()
+
+
+def is_image_nsfw(image_url: str, cache: bool = False) -> bool:
+    response = run_moderator(image_url=image_url, cache=cache)
+    return response["IsImageAdultClassified"]
+
+
+def check_image(image_url: str, cache: bool = False) -> None:
+    if is_image_nsfw(image_url=image_url, cache=cache):
+        raise ValueError(
+            "Your request was rejected as a result of our safety system. "
+            "Your input image may contain contents that are not allowed "
+            "by our safety system."
+        )
