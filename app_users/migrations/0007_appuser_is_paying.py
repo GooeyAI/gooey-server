0caf16b04ea85ef549a8de65d550bd96efc2d806
Dev Aggarwal <devxpy@gmail.com>
2023-10-13 15:37:18 +0530

    remove the set_is_paying_from_stripe_customer_id migration, and instead export data from stripe manually

--- app_users/migrations/0007_appuser_is_paying.py
+++ app_users/migrations/0007_appuser_is_paying.py
@@ -3,11 +3,6 @@
 from django.db import migrations, models
 
 
-def set_is_paying_from_stripe_customer_id(apps, schema_editor):
-    AppUser = apps.get_model("app_users", "appuser")
-    AppUser.objects.exclude(stripe_customer_id="").update(is_paying=True)
-
-
 class Migration(migrations.Migration):
     dependencies = [
         ("app_users", "0006_appuser_disable_safety_checker"),
@@ -18,8 +13,5 @@ class Migration(migrations.Migration):
             model_name="appuser",
             name="is_paying",
             field=models.BooleanField(default=False),
-        ),
-        migrations.RunPython(
-            set_is_paying_from_stripe_customer_id, migrations.RunPython.noop
         ),
     ]
