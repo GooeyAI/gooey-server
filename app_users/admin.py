a6e833b8ed3114a3224644b68008b1f32a3ae15f
clr-li <111320104+clr-li@users.noreply.github.com>
2023-10-26 01:02:46 -0700

    Change datetime format to follow Oct 19, 10:47 AM

--- app_users/admin.py
+++ app_users/admin.py
@@ -12,15 +12,12 @@ from bots.models import SavedRun
 class AppUserAdmin(admin.ModelAdmin):
     list_display = [
         "uid",
-        "display_name",
+        "name",
         "email",
         "phone_number",
-        "balance",
-        "is_paying",
-        "is_anonymous",
-        "is_disabled",
-        "created_at",
-        "upgraded_from_anonymous_at",
+        "bal",
+        "paid",
+        "created",
     ]
     search_fields = [
         "uid",
@@ -53,6 +50,22 @@ class AppUserAdmin(admin.ModelAdmin):
             instance_id=user.uid,
             show_add=False,
         )
+
+    @admin.display(description="Name")
+    def name(self, user: models.AppUser):
+        return user.display_name
+
+    @admin.display(description="Paid")
+    def paid(self, user: models.AppUser):
+        return user.is_paying
+
+    @admin.display(description="Bal")
+    def bal(self, user: models.AppUser):
+        return user.balance
+
+    @admin.display(description="Created")
+    def created(self, user: models.AppUser):
+        return user.created_at
 
     def open_in_firebase(self, user: models.AppUser):
         path = f"users/{user.uid}"
