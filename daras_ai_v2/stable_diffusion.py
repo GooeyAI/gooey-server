b2d03499200174c6e48f9cd44010173fe75d1e4b
Alexander Metzger <alexander.le@outlook.dk>
2023-11-26 22:53:26 -0800

    added reference image support via controlnets

--- daras_ai_v2/stable_diffusion.py
+++ daras_ai_v2/stable_diffusion.py
@@ -402,7 +402,7 @@ def controlnet(
     scheduler: str = None,
     prompt: str,
     num_outputs: int = 1,
-    init_image: str,
+    init_images: list[str] | str,
     num_inference_steps: int = 50,
     negative_prompt: str = None,
     guidance_scale: float = 7.5,
@@ -411,6 +411,8 @@ def controlnet(
 ):
     if isinstance(selected_controlnet_model, str):
         selected_controlnet_model = [selected_controlnet_model]
+    if isinstance(init_images, str):
+        init_images = [init_images] * len(selected_controlnet_model)
     prompt = add_prompt_prefix(prompt, selected_model)
     return call_sd_multi(
         "diffusion.controlnet",
@@ -432,7 +434,7 @@ def controlnet(
             "num_images_per_prompt": num_outputs,
             "num_inference_steps": num_inference_steps,
             "guidance_scale": guidance_scale,
-            "image": [init_image] * len(selected_controlnet_model),
+            "image": init_images,
             "controlnet_conditioning_scale": controlnet_conditioning_scale,
             # "strength": prompt_strength,
         },
