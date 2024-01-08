9203d7ae6d9308ae87c407ba7908d9787a1c3266
Dev Aggarwal <devxpy@gmail.com>
2024-01-08 20:46:02 +0530

    refactor

--- daras_ai_v2/exceptions.py
+++ daras_ai_v2/exceptions.py
@@ -24,7 +24,6 @@ def raise_for_status(resp: requests.Response):
     else:
         reason = resp.reason
 
-    response_body: str | bytes = ""
     try:
         response_body = str(resp.json())
     except JSONDecodeError:
@@ -32,14 +31,13 @@ def raise_for_status(resp: requests.Response):
             response_body = resp.text
         except ValueError:
             response_body = resp.content
-
     response_body = response_body[:500]  # truncate to at max 500 characters
 
     if 400 <= resp.status_code < 500:
-        http_error_msg = f"{resp.status_code} Client Error: {reason} | URL: {resp.url} | Response Body: {response_body!r}"
+        http_error_msg = f"{resp.status_code} Client Error: {reason} | URL: {resp.url} | Response: {response_body!r}"
 
     elif 500 <= resp.status_code < 600:
-        http_error_msg = f"{resp.status_code} Server Error: {reason} | URL: {resp.url} | Response Body: {response_body!r}"
+        http_error_msg = f"{resp.status_code} Server Error: {reason} | URL: {resp.url} | Response: {response_body!r}"
 
     if http_error_msg:
         raise HTTPError(http_error_msg, response=resp)
