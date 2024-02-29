b127d974a181f60a518333fefbe17399dda1b83c
Dev Aggarwal <devxpy@gmail.com>
2024-02-29 19:03:18 +0530

    refactor field_render repeated code

--- daras_ai_v2/field_render.py
+++ daras_ai_v2/field_render.py
@@ -4,30 +4,14 @@ from pydantic import BaseModel
 
 
 def field_title_desc(model: typing.Type[BaseModel], name: str) -> str:
-    field = model.__fields__[name]
-    return "\n".join(
-        filter(
-            None,
-            [field.field_info.title, field.field_info.description or ""],
-        )
-    )
+    return "\n".join(filter(None, [field_title(model, name), field_desc(model, name)]))
 
 
 def field_title(model: typing.Type[BaseModel], name: str) -> str:
     field = model.__fields__[name]
-    return "\n".join(
-        filter(
-            None,
-            [field.field_info.title],
-        )
-    )
+    return field.field_info.title
 
 
 def field_desc(model: typing.Type[BaseModel], name: str) -> str:
     field = model.__fields__[name]
-    return "\n".join(
-        filter(
-            None,
-            [field.field_info.description or ""],
-        )
-    )
+    return field.field_info.description or ""
