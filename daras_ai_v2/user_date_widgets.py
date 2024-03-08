fabfaeaa084a6a677c7eb1e66f7d79af78a08090
Dev Aggarwal <devxpy@gmail.com>
2024-03-08 14:42:25 +0530

    add renderLocalDt as a react useEffect for more consistent rendering

--- daras_ai_v2/user_date_widgets.py
+++ daras_ai_v2/user_date_widgets.py
@@ -1,80 +1,25 @@
 import datetime
-import json
-from typing import Any, Callable
+from typing import Any
 
-import gooey_ui as gui
+DEFAULT_DATE_OPTIONS = {
+    "day": "numeric",
+    "month": "short",
+}
+DEFAULT_TIME_OPTIONS = {
+    "hour": "numeric",
+    "hour12": True,
+    "minute": "numeric",
+}
 
 
-def js_dynamic_date(
+def render_local_dt_attrs(
     dt: datetime.datetime,
     *,
-    container: Callable = gui.caption,
     date_options: dict[str, Any] | None = None,
     time_options: dict[str, Any] | None = None,
 ):
-    timestamp_ms = dt.timestamp() * 1000
-    attrs = {"data-id-dynamic-date": str(timestamp_ms)}
-    if date_options:
-        attrs["data-id-date-options"] = json.dumps(date_options)
-    if time_options:
-        attrs["data-id-time-options"] = json.dumps(time_options)
-    container("Loading...", **attrs)
-
-
-def render_js_dynamic_dates():
-    default_date_options = {
-        "day": "numeric",
-        "month": "short",
+    return {
+        "renderLocalDt": dt.timestamp() * 1000,
+        "renderLocalDtDateOptions": date_options or DEFAULT_DATE_OPTIONS,
+        "renderLocalDtTimeOptions": time_options or DEFAULT_TIME_OPTIONS,
     }
-    default_time_options = {
-        "hour": "numeric",
-        "hour12": True,
-        "minute": "numeric",
-    }
-    gui.html(
-        # language=HTML
-        """
-<script>
-async function render_js_dynamic_dates() {
-    const defaultDateOptions = JSON.parse(`%(date_options_json)s`);
-    const defaultTimeOptions = JSON.parse(`%(time_options_json)s`);
-
-    function getOptions(elem, attr, defaultValue) {
-        options = elem.getAttribute(attr);
-        return options ? JSON.parse(options) : defaultValue;
-    }
-
-    document.querySelectorAll("[data-id-dynamic-date]").forEach(elem => {
-        let dateOptions = getOptions(elem, "data-id-date-options", defaultDateOptions);
-        let timeOptions = getOptions(elem, "data-id-time-options", defaultTimeOptions);
-        let date = new Date(parseFloat(elem.getAttribute("data-id-dynamic-date")));
-        let yearToShow = "";
-        if (date.getFullYear() != new Date().getFullYear()) {
-            yearToShow = " " + date.getFullYear().toString();
-        }
-        elem.children[0].innerHTML = `
-                ${date.toLocaleDateString("en-IN", dateOptions)}${yearToShow},
-                ${date.toLocaleTimeString("en-IN", timeOptions).toUpperCase()}
-        `;
-    });
-}
-window.waitUntilHydrated.then(render_js_dynamic_dates);
-window.addEventListener("hydrated", render_js_dynamic_dates);
-</script>
-        """
-        % {
-            "date_options_json": json.dumps(default_date_options),
-            "time_options_json": json.dumps(default_time_options),
-        },
-    )
-
-
-def re_render_js_dynamic_dates():
-    gui.html(
-        # language=HTML
-        """
-        <script>
-        render_js_dynamic_dates();
-        </script>
-        """,
-    )
