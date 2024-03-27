4323150cfa7be5d4f36a7b18a071e0788961201e
clr-li <111320104+clr-li@users.noreply.github.com>
2024-03-27 14:13:29 -0700

    More changes

--- tests/test_copilot_pricing.py
+++ tests/test_copilot_pricing.py
@@ -1,10 +1,10 @@
 from bots.models import AppUser
 from recipes.VideoBots import VideoBotsPage
-from usage_costs.models import UsageCost
-from bots.models import SavedRun
+from usage_costs.models import UsageCost, ModelPricing
+from bots.models import SavedRun, Workflow
 
 
-def test_copilot_get_raw_price_round_up():
+def test_copilot_get_raw_price_round_up(transactional_db):
     user = AppUser.objects.create(
         uid="test_user", is_paying=False, balance=1000, is_anonymous=False
     )
@@ -13,24 +13,29 @@ def test_copilot_get_raw_price_round_up():
         "num_outputs": 1,
     }
     bot_saved_run = SavedRun.objects.create(
-        parent_version=None,
-        workflow="Copilot",
+        workflow=Workflow.VIDEO_BOTS,
         run_id="test_run",
         uid=user.uid,
     )
 
-    pricing = 1
-    quantity = 1
-    unit_cost = 1
-    unit_quantity = 1
-    dollar_amount = 2.1
-
-    bot_usage_cost = UsageCost.objects.create(
-        saved_run=bot_saved_run,
-        pricing=pricing,
-        quantity=quantity,
-        unit_cost=unit_cost,
-        unit_quantity=unit_quantity,
-        dollar_amount=dollar_amount,
+    model_pricing = ModelPricing.objects.create(
+        model_id=1,
+        sku=1,
+        unit_cost=2.1,
+        unit_quantity=1,
+        category=1,
+        provider=1,
+        model_name="test_model",
     )
-    assert VideoBotsPage.get_raw_price(self=VideoBotsPage(), state=state) == 6
+
+    UsageCost.objects.create(
+        saved_run=bot_saved_run,
+        pricing=model_pricing,
+        quantity=1,
+        unit_cost=model_pricing.unit_cost,
+        unit_quantity=model_pricing.unit_quantity,
+        dollar_amount=model_pricing.unit_cost * 1 / model_pricing.unit_quantity,
+    )
+
+    video_bot_page = VideoBotsPage(run_user=user, request=dict())
+    assert video_bot_page.get_raw_price(state=state) == 6
