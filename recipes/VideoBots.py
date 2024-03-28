81853a0904f0d2c1902baf0c3c630c2a920cb6a2
Alexander Metzger <alexander.le@outlook.dk>
2024-03-28 11:11:47 -0700

    Drop _id

--- recipes/VideoBots.py
+++ recipes/VideoBots.py
@@ -192,7 +192,7 @@ class VideoBotsPage(BasePage):
         elevenlabs_similarity_boost: float | None
         azure_voice_name: str | None
         openai_voice_name: OPENAI_TTS_VOICES_T | None
-        openai_tts_model_id: OPENAI_TTS_MODELS_T | None
+        openai_tts_model: OPENAI_TTS_MODELS_T | None
 
         # llm settings
         selected_model: (
