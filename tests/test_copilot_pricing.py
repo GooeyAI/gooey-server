3d4cd250646fea2029b2a5597e90d7e1c385614d
clr-li <111320104+clr-li@users.noreply.github.com>
2024-04-04 12:31:00 -0700

    Removed bad test

--- tests/test_copilot_pricing.py
+++ tests/test_copilot_pricing.py
@@ -1,6 +1,5 @@
 import pytest
 import math
-from starlette.requests import Request
 from bots.models import AppUser
 from recipes.VideoBots import VideoBotsPage
 from usage_costs.models import UsageCost, ModelPricing
@@ -8,7 +7,6 @@ from bots.models import SavedRun, Workflow
 from gooey_ui.state import set_query_params
 from starlette.testclient import TestClient
 from server import app
-from django.db.models import Sum
 
 client = TestClient(app)
 
@@ -50,40 +48,3 @@ def test_copilot_get_raw_price_round_up(transactional_db):
     copilot_page = VideoBotsPage(run_user=user)
     set_query_params({"run_id": bot_saved_run.run_id or "", "uid": user.uid or ""})
     assert copilot_page.get_price_roundoff(state=state) == 6
-
-
-@pytest.mark.django_db
-def test_get_raw_price(transactional_db, mock_gui_runner, force_authentication):
-    user = AppUser.objects.create(
-        uid="test_user", is_paying=False, balance=1000, is_anonymous=False
-    )
-    bot_saved_run = SavedRun.objects.create(
-        workflow=Workflow.VIDEO_BOTS,
-        run_id="test_run",
-        uid=user.uid,
-    )
-
-    state = {
-        "input_prompt": "You are Farmer.CHAT - an intelligent AI assistant built by Gooey.AI and DigitalGreen.org to help Indian farmers and agriculture agents. Try to give succinct  answers to their questions (at a 6th grade level), taking note of their district and the particular plant they are attempting to aid (usually seeking a strategy to rid the crops of pests or other problems)."
-    }
-
-    r = client.post(
-        f"/v2/{VideoBotsPage.slug_versions[0]}/?run_id={bot_saved_run.run_id}&uid={user.uid}",
-        json=VideoBotsPage.get_example_request_body(state),
-        headers={"Authorization": f"Token None"},
-        allow_redirects=False,
-    )
-    assert r.status_code == 200
-    assert r.text
-
-    res = r.json()
-
-    usage_cost_dollar_amt = math.ceil(
-        UsageCost.objects.filter(saved_run__run_id=res["run_id"]).aggregate(
-            Sum("dollar_amount")
-        )["dollar_amount__sum"]
-    )
-
-    assert usage_cost_dollar_amt == VideoBotsPage(run_user=user).get_raw_price(
-        state=state
-    )
