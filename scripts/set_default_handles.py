a178f01e372a0a0a71630a878f371bf99d3c18ea
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-04-08 18:56:50 +0530

    Add script to set default handles

---
+++ scripts/set_default_handles.py
@@ -0,0 +1,20 @@
+from loguru import logger
+
+from app_users.models import AppUser
+from handles.models import Handle
+
+
+def run():
+    registered_users = AppUser.objects.filter(
+        handle__isnull=True,
+        is_anonymous=False,
+        email__isnull=False,
+    )
+
+    for user in registered_users:
+        if handle := Handle.create_default_for_user(user):
+            user.handle = handle
+            user.save()
+        else:
+            # for-else runs when "break" is not hit in the loop
+            logger.warning(f"unable to find acceptable handle for user: {user}")
