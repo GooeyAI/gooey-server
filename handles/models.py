cec69131303b9717c7de61a1cfa3b2f7a487210f
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-04-09 20:41:00 +0530

    Morph default handle logic depending on the email

--- handles/models.py
+++ handles/models.py
@@ -30,6 +30,30 @@ BASE_HANDLE_BLACKLIST = [
     "contact",
     "sales",
 ]
+COMMON_EMAIL_DOMAINS = [
+    "gmail.com",
+    "googlemail.com",
+    "outlook.com",
+    "hotmail.com",
+    "yahoo.com",
+    "myyahoo.com",
+    "proton.me",
+    "protonmail.com",
+    "aol.com",
+    "aim.com",
+    "icloud.com",
+    "yandex.com",
+    "tutanota.com",
+    "tuta.com",
+    "tutanota.de",
+    "tutamail.com",
+    "tuta.io",
+    "keemail.me",
+    "zohomail.com",
+]
+PRIVATE_EMAIL_DOMAINS = [
+    "privaterelay.appleid.com",
+]
 
 validate_handle_regex = RegexValidator(
     regex=HANDLE_REGEX,
@@ -130,26 +154,58 @@ class Handle(models.Model):
 
 
 def _make_handle_from(name):
-    name = name.lower()
-    name = "".join(
-        m.capitalize() for m in re.findall(HANDLE_ALLOWED_CHARS, name)
-    )  # find groups of valid chars
+    allowed_groups = [m for m in re.findall(HANDLE_ALLOWED_CHARS, name)]
+    if len(allowed_groups) == 1:
+        # as it is
+        name = allowed_groups[0]
+    else:
+        # capitalize first letter of each valid group
+        name = "".join(capitalize_first(group) for group in allowed_groups)
+
     name = name[:HANDLE_MAX_LENGTH]
-    name = name.rstrip("-_")
+    name = name.rstrip("-_.")
     return name
 
 
 def _generate_handle_options(user):
-    email_handle = _make_handle_from(user.email.split("@")[0]) if user.email else ""
-    if email_handle:
-        yield email_handle
+    email_domain = user.email.split("@")[1] if user.email else None
 
-    if user.display_name:
-        yield _make_handle_from(user.display_name)
+    match email_domain:
+        case _ if email_domain in COMMON_EMAIL_DOMAINS:
+            email_prefix = _make_handle_from(user.email.split("@")[0])
+            if email_prefix:
+                yield email_prefix
 
-    if email_handle:
-        for i in range(10):
-            yield f"{email_handle}{i}"
+            if user.display_name:
+                yield _make_handle_from(user.display_name)
+
+            if email_prefix:
+                for i in range(1, 10):
+                    yield f"{email_prefix[:HANDLE_MAX_LENGTH-1]}{i}"
+
+        case _ if email_domain in PRIVATE_EMAIL_DOMAINS:
+            if user.display_name:
+                name_handle = _make_handle_from(user.display_name)
+                yield name_handle
+                for i in range(1, 10):
+                    yield f"{name_handle[:HANDLE_MAX_LENGTH-1]}{i}"
+
+        case _ if email_domain:
+            if user.display_name:
+                name_handle = _make_handle_from(user.display_name)
+                yield name_handle
+
+            email_prefix = user.email.split("@")[0]
+            domain_part = email_domain.split(".")[0]
+            email_handle = _make_handle_from(
+                capitalize_first(email_prefix) + capitalize_first(domain_part)
+            )
+            yield email_handle
+            for i in range(1, 10):
+                yield f"{email_handle[:HANDLE_MAX_LENGTH-1]}{i}"
+
+        case _:
+            pass
 
 
 def _attempt_create_handle(handle_name):
@@ -162,3 +218,7 @@ def _attempt_create_handle(handle_name):
         return handle
     except (IntegrityError, ValidationError):
         return None
+
+
+def capitalize_first(s: str) -> str:
+    return s[0].upper() + s[1:] if s else ""
