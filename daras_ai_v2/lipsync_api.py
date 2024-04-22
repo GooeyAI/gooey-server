912248db2cb4ac5603947e6ed0e718c402f17a39
Alexander Metzger <alexander.le@outlook.dk>
2024-04-22 01:10:58 -0700

    functionality + basic ui

--- daras_ai_v2/lipsync_api.py
+++ daras_ai_v2/lipsync_api.py
@@ -3,6 +3,53 @@ from loguru import logger
 from daras_ai_v2.exceptions import UserError, GPUError
 from daras_ai_v2.gpu_server import call_celery_task_outfile
 
+from pydantic import HttpUrl
+
+
+def sadtalker(
+    *,
+    source_image: HttpUrl,
+    driven_audio: HttpUrl,
+    pose_style: int,
+    ref_eyeblink: HttpUrl | None = None,
+    ref_pose: HttpUrl | None = None,
+    batch_size: int = 2,
+    size: int = 256,
+    expression_scale: float = 1.0,
+    input_yaw: list[int] | None = None,
+    input_pitch: list[int] | None = None,
+    input_roll: list[int] | None = None,
+    enhancer: str | None = None,
+    background_enhancer: str | None = None,
+    face3dvis: bool = False,
+    still: bool = False,
+    preprocess: str = "crop",
+):
+    return call_celery_task_outfile(
+        "lipsync.sadtalker",
+        pipeline=dict(model_id="sadtalker"),
+        inputs=dict(
+            source_image=source_image,
+            driven_audio=driven_audio,
+            pose_style=pose_style,
+            ref_eyeblink=ref_eyeblink,
+            ref_pose=ref_pose,
+            batch_size=batch_size,
+            size=size,
+            expression_scale=expression_scale,
+            input_yaw=input_yaw,
+            input_pitch=input_pitch,
+            input_roll=input_roll,
+            enhancer=enhancer,
+            background_enhancer=background_enhancer,
+            face3dvis=face3dvis,
+            still=still,
+            preprocess=preprocess,
+        ),
+        content_type="video/mp4",
+        filename=f"gooey.ai lipsync.mp4",
+    )[0]
+
 
 def wav2lip(*, face: str, audio: str, pads: tuple[int, int, int, int]) -> bytes:
     try:
--- daras_ai_v2/lipsync_settings_widgets.py
+++ daras_ai_v2/lipsync_settings_widgets.py
@@ -1,7 +1,71 @@
 import gooey_ui as st
+from enum import Enum
+
+
+class LipsyncModel(Enum):
+    Wav2Lip = "Wav2Lip"
+    SadTalker = "SadTalker"
 
 
 def lipsync_settings():
+    model = st.selectbox(
+        "Select the Lipsync Model",
+        LipsyncModel._member_names_,
+        key="lipsync_model",
+        format_func=lambda x: LipsyncModel[x].value,  # type: ignore
+    )
+    if model == LipsyncModel.Wav2Lip.name:
+        wav2lip_settings()
+    elif model == LipsyncModel.SadTalker.name:
+        sadtalker_settings()
+
+
+def sadtalker_settings():
+    st.session_state.setdefault("pose_style", 0)
+    st.slider("Pose Style", min_value=0, max_value=45, key="pose_style")
+    st.file_uploader("Reference Eyeblink", key="ref_eyeblink", accept=[".mp4"])
+    if not st.session_state.get("ref_eyeblink"):
+        st.session_state["ref_eyeblink"] = None
+    st.file_uploader("Reference Pose", key="ref_pose", accept=[".mp4"])
+    if not st.session_state.get("ref_pose"):
+        st.session_state["ref_pose"] = None
+    st.session_state.setdefault("batch_size", 2)
+    st.number_input("Batch Size", min_value=1, key="batch_size")
+    st.session_state.setdefault("size", 256)
+    st.number_input("Image Size", min_value=1, key="size")
+    st.session_state.setdefault("expression_scale", 1.0)
+    st.number_input("Expression Scale", key="expression_scale")
+    st.session_state["__input_yaw"] = ",".join(st.session_state.get("input_yaw") or [])
+    input_yaw = st.text_input("Input Yaw (comma separated)", key="__input_yaw")
+    st.session_state["input_yaw"] = (
+        list(map(int, input_yaw.split(","))) if input_yaw.strip() else None
+    )
+    st.session_state["__input_pitch"] = ",".join(
+        st.session_state.get("input_pitch") or []
+    )
+    input_pitch = st.text_input("Input Pitch (comma separated)", key="__input_pitch")
+    st.session_state["input_pitch"] = (
+        list(map(int, input_pitch.split(","))) if input_pitch.strip() else None
+    )
+    st.session_state["__input_roll"] = ",".join(
+        st.session_state.get("input_roll") or []
+    )
+    input_roll = st.text_input("Input Roll (comma separated)", key="__input_roll")
+    st.session_state["input_roll"] = (
+        list(map(int, input_roll.split(","))) if input_roll.strip() else None
+    )
+    st.selectbox("Face Enhancer", [None, "gfpgan", "RestoreFormer"], key="enhancer")
+    st.selectbox("Background Enhancer", [None, "realesrgan"], key="background_enhancer")
+    st.checkbox("Generate 3D Face and 3D Landmarks", key="face3dvis")
+    st.checkbox("Still (fewer head motion, works with preprocess 'full')", key="still")
+    st.selectbox(
+        "Preprocess",
+        ["crop", "extcrop", "resize", "full", "extfull"],
+        key="preprocess",
+    )
+
+
+def wav2lip_settings():
     st.write("##### ⌖ Lipsync Face Padding")
     st.caption(
         "Adjust the detected face bounding box. Often leads to improved results. Recommended to give at least 10 padding for the chin region."
