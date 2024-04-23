0bee08c7cad8209114c8646e2983027dfc988333
clr-li <111320104+clr-li@users.noreply.github.com>
2024-04-22 23:29:57 -0700

    Record cost for groq

--- daras_ai_v2/language_model.py
+++ daras_ai_v2/language_model.py
@@ -649,7 +649,6 @@ def _run_anthropic_chat(
         stop_sequences=stop,
         temperature=temperature,
     )
-
     record_cost_auto(
         model=model,
         sku=ModelSku.llm_prompt,
@@ -888,6 +887,9 @@ def _run_groq_chat(
     avoid_repetition: bool,
     stop: list[str] | None,
 ):
+    from usage_costs.cost_utils import record_cost_auto
+    from usage_costs.models import ModelSku
+
     data = {
         "model": model,
         "messages": messages,
@@ -908,6 +910,15 @@ def _run_groq_chat(
     )
     raise_for_status(r)
     out = r.json()
+
+    record_cost_auto(
+        model=model, sku=ModelSku.llm_prompt, quantity=out["usage"]["prompt_tokens"]
+    )
+    record_cost_auto(
+        model=model,
+        sku=ModelSku.llm_completion,
+        quantity=out["usage"]["completion_tokens"],
+    )
     return [choice["message"] for choice in out["choices"]]
 
 
