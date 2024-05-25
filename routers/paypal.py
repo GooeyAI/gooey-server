d1c62dd2b39479f236ca723ea8b41bc3b708b323
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-05-26 02:32:23 +0530

    Fix typo

--- routers/paypal.py
+++ routers/paypal.py
@@ -249,7 +249,7 @@ def _handle_sale_completed(event: SaleCompletedEvent):
     if not user.is_paying:
         user.is_paying = True
         user.save(update_fields=["is_paying"])
-    if user.should_send_monthly_spending_notification_email():
+    if user.should_send_monthly_spending_notification():
         send_monthly_spending_notification_email(user)
 
 
