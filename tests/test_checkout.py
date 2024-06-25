85874ee1cbedf66803cc1d50a7fc6ee28db071d6
Dev Aggarwal <devxpy@gmail.com>
2024-06-25 19:11:16 +0530

    github actions python tests skip tests if env var missing allow runing the project without azure and 11 labs keys remove AZURE_TTS_ENDPOINT env var

--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -2,6 +2,7 @@ import pytest
 from fastapi.testclient import TestClient
 
 from app_users.models import AppUser
+from daras_ai_v2 import settings
 from daras_ai_v2.billing import create_stripe_checkout_session
 from gooey_ui import RedirectException
 from payments.plans import PricingPlan
@@ -10,6 +11,7 @@ from server import app
 client = TestClient(app)
 
 
+@pytest.mark.skipif(not settings.STRIPE_SECRET_KEY, reason="No stripe secret")
 @pytest.mark.parametrize("plan", PricingPlan)
 def test_create_checkout_session(
     plan: PricingPlan, transactional_db, force_authentication: AppUser
