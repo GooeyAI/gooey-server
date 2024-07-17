d8616c8e5dd8dcd1bc2edc840422f1eca15c88ba
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-07-17 18:46:15 +0530

    Add confirmation modals for removing members, move danger zone to edit

--- orgs/views.py
+++ orgs/views.py
@@ -33,24 +33,31 @@ def render_org_by_membership(membership: OrgMembership):
     org = membership.org
     current_user = membership.user
 
-    with st.div(className="d-flex justify-content-between"):
+    with st.div(
+        className="d-xs-block d-sm-flex flex-row-reverse justify-content-between"
+    ):
+        with st.div(className="d-flex justify-content-center align-items-center"):
+            if membership.can_edit_org_metadata():
+                org_edit_modal = Modal("Edit Org", key="edit-org-modal")
+                if org_edit_modal.is_open():
+                    with org_edit_modal.container():
+                        render_org_edit_view_by_membership(
+                            membership, modal=org_edit_modal
+                        )
+
+                if st.button(f"{icons.edit} Edit", type="secondary"):
+                    org_edit_modal.open()
+
         with st.div(className="d-flex align-items-center"):
             st.image(
                 org.logo or DEFAULT_ORG_LOGO,
                 className="my-0 me-2",
                 style={"width": "128px", "height": "128px", "object-fit": "contain"},
             )
-            st.write(f"# {org.name}")
-
-        if membership.can_edit_org_metadata():
-            org_edit_modal = Modal("Edit Org", key="edit-org-modal")
-            if org_edit_modal.is_open():
-                with org_edit_modal.container():
-                    render_org_edit_view(membership.org, modal=org_edit_modal)
-
-            with st.div(className="d-flex align-items-center"):
-                if st.button(f"{icons.edit} Edit", className="btn btn-theme"):
-                    org_edit_modal.open()
+            with st.div(className="d-flex flex-column justify-content-center"):
+                st.write(f"# {org.name}")
+                if org.domain_name:
+                    st.write(f"Domain: `@{org.domain_name}`", className="text-muted")
 
     with st.div(className="mt-4"):
         with st.div(className="d-flex justify-content-between align-items-center"):
@@ -73,40 +80,55 @@ def render_org_by_membership(membership: OrgMembership):
         render_pending_invitations_list(org=org, current_user=current_user)
 
     with st.div(className="mt-4"):
-        st.write("# Danger Zone")
+        if membership.role != OrgRole.OWNER:
+            # Owners can't leave! They can only delete
+            org_leave_modal = Modal("Leave Org", key="leave-org-modal")
+            if org_leave_modal.is_open():
+                with org_leave_modal.container():
+                    render_org_leave_view_by_membership(
+                        membership, modal=org_leave_modal
+                    )
 
-        if membership.role == OrgRole.OWNER:
-            # Owner can't leave! Only delete
-            if st.button(
-                "Delete Org",
-                className="btn btn-theme bg-danger border-danger text-white",
-            ):
-                org.soft_delete()
-                st.experimental_rerun()
-        else:
             if st.button(
                 "Leave Org",
                 className="btn btn-theme bg-danger border-danger text-white",
             ):
-                org.members.remove(current_user)
-                st.experimental_rerun()
+                org_leave_modal.open()
 
 
 def render_org_creation_view(user: AppUser):
     st.write(f"# {icons.company} Create an Org", unsafe_allow_html=True)
     name = st.text_input("Team Name")
-    logo = st.file_uploader("Org Logo", accept=["image/*"])
+    logo = st.file_uploader("Logo", accept=["image/*"])
+    domain_name = st.text_input("Domain Name (Optional)", placeholder="e.g. gooey.ai")
+    if domain_name:
+        st.caption(f"Add any user with `@{domain_name}` email to this organization.")
 
     if st.button("Create"):
-        Org.objects.create_org(created_by=user, name=name, logo=logo)
-        st.experimental_rerun()
+        try:
+            Org.objects.create_org(
+                created_by=user, name=name, logo=logo, domain_name=domain_name
+            )
+        except ValidationError as e:
+            st.write(", ".join(e.messages), className="text-danger")
+        else:
+            st.experimental_rerun()
 
 
-def render_org_edit_view(org: Org, *, modal: Modal):
-    org.name = st.text_input("Org Name", value=org.name)
-    org.logo = st.file_uploader("Org Logo", accept=["image/*"], value=org.logo)
+def render_org_edit_view_by_membership(membership: OrgMembership, *, modal: Modal):
+    org = membership.org
 
-    if st.button("Save"):
+    org.name = st.text_input("Team Name", value=org.name)
+    org.logo = st.file_uploader("Logo", accept=["image/*"], value=org.logo)
+    org.domain_name = st.text_input(
+        "Domain Name (Optional)", placeholder="e.g. gooey.ai", value=org.domain_name
+    )
+    if org.domain_name:
+        st.caption(
+            f"Add any user with `@{org.domain_name}` email to this organization."
+        )
+
+    if st.button("Save", className="w-100", type="primary"):
         try:
             org.full_clean()
         except ValidationError as e:
@@ -116,74 +138,142 @@ def render_org_edit_view(org: Org, *, modal: Modal):
             org.save()
             modal.close()
 
+    if membership.can_delete_org() or membership.can_transfer_ownership():
+        st.write("---")
+        render_danger_zone_by_membership(membership)
+
+
+def render_danger_zone_by_membership(membership: OrgMembership):
+    st.write("### Danger Zone", className="d-block my-2")
+
+    if membership.can_delete_org():
+        org_deletion_modal = Modal("Delete Organization", key="delete-org-modal")
+        if org_deletion_modal.is_open():
+            with org_deletion_modal.container():
+                render_org_deletion_view_by_membership(
+                    membership, modal=org_deletion_modal
+                )
+
+        with st.div(className="d-flex justify-content-between align-items-center"):
+            st.write("Delete Organization")
+            if st.button(
+                f"{icons.delete} Delete",
+                className="btn btn-theme py-2 bg-danger border-danger text-white",
+            ):
+                org_deletion_modal.open()
+
+    if membership.can_transfer_ownership():
+        with st.div(className="d-flex justify-content-between align-items-center"):
+            st.write("Transfer Ownership")
+            if st.button(
+                f"{icons.transfer} Transfer",
+                className="btn btn-theme py-2 bg-danger border-danger text-light",
+                unsafe_allow_html=True,
+            ):
+                m.role = OrgRole.OWNER
+                membership.role = OrgRole.ADMIN
+                with transaction.atomic():
+                    m.save()
+                    membership.save()
+                st.experimental_rerun()
+
+
+def render_org_deletion_view_by_membership(membership: OrgMembership, *, modal: Modal):
+    st.write(
+        f"Are you sure you want to delete **{membership.org.name}**? This action is irreversible."
+    )
+
+    with st.div(className="d-flex"):
+        if st.button(
+            "Cancel", type="secondary", className="border-danger text-danger w-50"
+        ):
+            modal.close()
+
+        if st.button(
+            "Delete", className="btn btn-theme bg-danger border-danger text-light w-50"
+        ):
+            membership.org.soft_delete()
+            modal.close()
+
+
+def render_org_leave_view_by_membership(membership: OrgMembership, *, modal: Modal):
+    st.write("Are you sure you want to leave this organization?")
+
+    if st.button("Cancel", type="secondary", className="border-danger text-danger"):
+        modal.close()
+
+    if st.button("Leave", className="btn btn-theme bg-danger border-danger text-light"):
+        membership.org.members.remove(membership.user)
+        modal.close()
+
 
 def render_members_list(org: Org, current_membership: OrgMembership):
-    with st.raw_table(["Name", "Role", f"{icons.time} Since", ""]):
-        for m in org.memberships.all().order_by("created_at"):
-            name_val = m.user.display_name or m.user.first_name()
-            if current_membership == m:
-                name_val += " (You)"
-            with st.tag("tr"):
-                with st.tag("td"):
-                    if m.user.handle_id:
-                        with st.link(to=m.user.handle.get_app_url()):
-                            st.html(html_lib.escape(name_val))
-                    else:
-                        st.html(html_lib.escape(name_val))
-                with st.tag("td"):
-                    st.html(html_lib.escape(m.get_role_display()))
-                with st.tag("td"):
-                    st.html(m.created_at.strftime("%b %d, %Y"))
-                with st.tag("td", className="text-end"):
-                    render_role_change_buttons(m, current_membership=current_membership)
-                    render_membership_buttons(m, current_membership=current_membership)
+    with st.tag("table", className="table table-responsive"):
+        with st.tag("thead"), st.tag("tr"):
+            with st.tag("th", scope="col"):
+                st.html("Name")
+            with st.tag("th", scope="col"):
+                st.html("Role")
+            with st.tag("th", scope="col"):
+                st.html(f"{icons.time} Since")
+            with st.tag("th", scope="col"):
+                st.html("")
+
+        with st.tag("tbody"):
+            for m in org.memberships.all().order_by("created_at"):
+                with st.tag("tr"):
+                    with st.tag("td"):
+                        name = format_user_name(
+                            m.user, current_user=current_membership.user
+                        )
+                        if m.user.handle_id:
+                            with st.link(to=m.user.handle.get_app_url()):
+                                st.html(html_lib.escape(name))
+                        else:
+                            st.html(html_lib.escape(name))
+                    with st.tag("td"):
+                        st.html(m.get_role_display())
+                    with st.tag("td"):
+                        st.html(m.created_at.strftime("%b %d, %Y"))
+                    with st.tag("td", className="text-end"):
+                        render_membership_actions(
+                            m, current_membership=current_membership
+                        )
 
 
-def render_role_change_buttons(m: OrgMembership, current_membership: OrgMembership):
-    if current_membership.can_change_role(m):
-        if m.role == OrgRole.MEMBER:
-            if st.button(
-                f"{icons.admin} Make Admin",
-                className="btn btn-theme btn-sm my-0 py-0",
-                unsafe_allow_html=True,
-            ):
-                m.role = OrgRole.ADMIN
-                m.save()
-                st.experimental_rerun()
-        if m.role == OrgRole.ADMIN:
-            if st.button(
-                f"{icons.admin} Remove Admin",
-                className="btn btn-theme btn-sm my-0 py-0",
-                unsafe_allow_html=True,
-            ):
-                m.role = OrgRole.MEMBER
-                m.save()
-                st.experimental_rerun()
-
-    if current_membership.can_transfer_ownership(m):
-        if st.button(
-            f"{icons.transfer_ownership} Transfer Ownership",
-            className="btn btn-theme btn-sm my-0 py-0 bg-danger border-danger text-light",
-            unsafe_allow_html=True,
-        ):
-            m.role = OrgRole.OWNER
-            current_membership.role = OrgRole.ADMIN
-            with transaction.atomic():
-                m.save()
-                current_membership.save()
-            st.experimental_rerun()
-
-
-def render_membership_buttons(m: OrgMembership, current_membership: OrgMembership):
+def render_membership_actions(m: OrgMembership, current_membership: OrgMembership):
     if current_membership.can_kick(m):
+        member_deletion_modal = Modal(
+            "Remove Member", key=f"remove-member-{m.pk}-modal"
+        )
+        if member_deletion_modal.is_open():
+            with member_deletion_modal.container():
+                render_member_deletion_view(m, modal=member_deletion_modal)
+
         if st.button(
             f"{icons.remove_user} Remove",
             className="btn btn-theme btn-sm my-0 py-0 bg-danger border-danger text-light",
             unsafe_allow_html=True,
         ):
-            # TODO: soft-delete
-            m.delete()
-            st.experimental_rerun()
+            member_deletion_modal.open()
+
+
+def render_member_deletion_view(membership: OrgMembership, modal: Modal):
+    st.write(
+        f"Are you sure you want to remove **{format_user_name(membership.user)}** from **{membership.org.name}**?"
+    )
+
+    with st.div(className="d-flex"):
+        if st.button(
+            "Cancel", type="secondary", className="border-danger text-danger w-50"
+        ):
+            modal.close()
+
+        if st.button(
+            "Remove", className="btn btn-theme bg-danger border-danger text-light w-50"
+        ):
+            membership.delete()
+            modal.close()
 
 
 def render_pending_invitations_list(org: Org, current_user: AppUser):
@@ -192,19 +282,30 @@ def render_pending_invitations_list(org: Org, current_user: AppUser):
         return
 
     st.write("## Pending")
-    table = st.raw_table(["Email", "Invited By", f"{icons.time} Invited on"])
-    with table:
-        for invite in pending_invitations:
-            inviter_name = invite.inviter.display_name or invite.inviter.first_name()
-            if current_user == invite.inviter:
-                inviter_name += " (You)"
-            st.table_row(
-                [
-                    invite.invitee_email,
-                    inviter_name,
-                    invite.created_at.strftime("%b %d, %Y"),
-                ]
-            )
+    with st.tag("table", className="table table-responsive"):
+        with st.tag("thead"), st.tag("tr"):
+            with st.tag("th", scope="col"):
+                st.html("Email")
+            with st.tag("th", scope="col"):
+                st.html("Invited By")
+            with st.tag("th", scope="col"):
+                st.html(f"{icons.time} Invited on")
+
+        with st.tag("tbody"):
+            for invite in pending_invitations:
+                with st.tag("tr", className="text-break"):
+                    with st.tag("td"):
+                        st.html(html_lib.escape(invite.invitee_email))
+                    with st.tag("td"):
+                        st.html(
+                            html_lib.escape(
+                                format_user_name(
+                                    invite.inviter, current_user=current_user
+                                )
+                            )
+                        )
+                    with st.tag("td"):
+                        st.html(invite.created_at.strftime("%b %d, %Y"))
 
 
 def render_invite_creation_view(org: Org, inviter: AppUser, modal: Modal):
@@ -213,6 +314,14 @@ def render_invite_creation_view(org: Org, inviter: AppUser, modal: Modal):
     if st.button(f"{icons.add_user} Invite", type="primary", unsafe_allow_html=True):
         try:
             org.invite_user(invitee_email=email, inviter=inviter, role=OrgRole.MEMBER)
-            modal.close()
         except ValidationError as e:
             st.write(", ".join(e.messages), className="text-danger")
+        else:
+            modal.close()
+
+
+def format_user_name(user: AppUser, current_user: AppUser | None = None):
+    name = user.display_name or user.first_name()
+    if current_user and user == current_user:
+        name += " (You)"
+    return name
