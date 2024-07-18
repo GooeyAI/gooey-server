c60f520caed5cac1ce3724b18d7bf36a8ed43ed9
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-07-18 16:36:08 +0530

    make use of django-safedelete library

--- orgs/admin.py
+++ orgs/admin.py
@@ -1,19 +1,105 @@
 from django.contrib import admin
+from safedelete.admin import SafeDeleteAdmin, SafeDeleteAdminFilter
 
-from .models import Org, OrgQuerySet, OrgInvitation, OrgMembership
+from bots.admin_links import change_obj_url
+from orgs.models import Org, OrgMembership, OrgInvitation
+
+
+class OrgMembershipInline(admin.TabularInline):
+    model = OrgMembership
+    extra = 0
+    show_change_link = True
+    fields = ["user", "role", "created_at", "updated_at"]
+    readonly_fields = ["created_at", "updated_at"]
+    ordering = ["-created_at"]
+    can_delete = False
+    show_change_link = True
+
+
+class OrgInvitationInline(admin.TabularInline):
+    model = OrgInvitation
+    extra = 0
+    show_change_link = True
+    fields = [
+        "invitee_email",
+        "inviter",
+        "role",
+        "status",
+        "auto_accepted",
+        "created_at",
+        "updated_at",
+    ]
+    readonly_fields = ["auto_accepted", "created_at", "updated_at"]
+    ordering = ["status", "-created_at"]
+    can_delete = False
+    show_change_link = True
 
 
 @admin.register(Org)
-class OrgAdmin(admin.ModelAdmin):
-    def get_queryset(self, request):
-        return OrgQuerySet(self.model).all()
+class OrgAdmin(SafeDeleteAdmin):
+    list_display = [
+        "name",
+        "domain_name",
+        "created_at",
+        "updated_at",
+    ] + list(SafeDeleteAdmin.list_display)
+    list_filter = [SafeDeleteAdminFilter] + list(SafeDeleteAdmin.list_filter)
+    fields = ["name", "domain_name", "created_by", "created_at", "updated_at"]
+    search_fields = ["name", "domain_name"]
+    readonly_fields = ["created_at", "updated_at"]
+    inlines = [OrgMembershipInline, OrgInvitationInline]
+    ordering = ["-created_at"]
 
 
 @admin.register(OrgMembership)
-class OrgMembershipAdmin(admin.ModelAdmin):
-    pass
+class OrgMembershipAdmin(SafeDeleteAdmin):
+    list_display = [
+        "user",
+        "org",
+        "role",
+        "created_at",
+        "updated_at",
+    ] + list(SafeDeleteAdmin.list_display)
+    list_filter = ["org", "role", SafeDeleteAdminFilter] + list(
+        SafeDeleteAdmin.list_filter
+    )
+
+    def get_readonly_fields(
+        self, request: "HttpRequest", obj: OrgMembership | None = None
+    ) -> list[str]:
+        readonly_fields = list(super().get_readonly_fields(request, obj))
+        if obj and obj.org and obj.org.deleted:
+            return readonly_fields + ["deleted_org"]
+        else:
+            return readonly_fields
+
+    @admin.display
+    def deleted_org(self, obj):
+        org = Org.deleted_objects.get(pk=obj.org_id)
+        return change_obj_url(org)
 
 
 @admin.register(OrgInvitation)
-class OrgInvitationAdmin(admin.ModelAdmin):
-    pass
+class OrgInvitationAdmin(SafeDeleteAdmin):
+    fields = [
+        "org",
+        "invitee_email",
+        "inviter",
+        "role",
+        "status",
+        "auto_accepted",
+        "created_at",
+        "updated_at",
+    ]
+    list_display = [
+        "org",
+        "invitee_email",
+        "inviter",
+        "status",
+        "created_at",
+        "updated_at",
+    ] + list(SafeDeleteAdmin.list_display)
+    list_filter = ["org", "inviter", "role", SafeDeleteAdminFilter] + list(
+        SafeDeleteAdmin.list_filter
+    )
+    readonly_fields = ["auto_accepted"]
