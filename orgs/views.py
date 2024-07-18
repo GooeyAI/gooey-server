fac8b8ac2cd9edf1e79c9ed6a5d105b18b054c01
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-07-18 18:30:05 +0530

    python magic to reuse same form for create/edit org

--- orgs/views.py
+++ orgs/views.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 import html as html_lib
+
 from django.core.exceptions import ValidationError
 
 import gooey_ui as st
@@ -93,37 +96,15 @@ def render_org_by_membership(membership: OrgMembership):
             org_leave_modal.open()
 
 
-#     if membership.can_transfer_ownership():
-#         transfer_ownership_modal = Modal("Transfer Ownership", key="transfer-ownership")
-#         if transfer_ownership_modal.is_open():
-#             with transfer_ownership_modal.container():
-#                 render_transfer_ownership_view_by_membership(
-#                     membership, modal=transfer_ownership_modal
-#                 )
-#
-#         with st.div(className="d-flex justify-content-between align-items-center"):
-#             st.write("Transfer Ownership")
-#             if st.button(
-#                 f"{icons.transfer} Transfer",
-#                 className="btn btn-theme py-2 bg-danger border-danger text-light",
-#                 unsafe_allow_html=True,
-#             ):
-#                 transfer_ownership_modal.open()
-#
-
-
 def render_org_creation_view(user: AppUser):
     st.write(f"# {icons.company} Create an Org", unsafe_allow_html=True)
-    name = st.text_input("Team Name")
-    logo = st.file_uploader("Logo", accept=["image/*"])
-    domain_name = st.text_input("Domain Name (Optional)", placeholder="e.g. gooey.ai")
-    if domain_name:
-        st.caption(f"Add any user with `@{domain_name}` email to this organization.")
+    org_fields = render_org_create_or_edit_form()
 
     if st.button("Create"):
         try:
             Org.objects.create_org(
-                created_by=user, name=name, logo=logo, domain_name=domain_name
+                created_by=user,
+                **org_fields,
             )
         except ValidationError as e:
             st.write(", ".join(e.messages), className="text-danger")
@@ -133,16 +114,7 @@ def render_org_creation_view(user: AppUser):
 
 def render_org_edit_view_by_membership(membership: OrgMembership, *, modal: Modal):
     org = membership.org
-
-    org.name = st.text_input("Team Name", value=org.name)
-    org.logo = st.file_uploader("Logo", accept=["image/*"], value=org.logo)
-    org.domain_name = st.text_input(
-        "Domain Name (Optional)", placeholder="e.g. gooey.ai", value=org.domain_name
-    )
-    if org.domain_name:
-        st.caption(
-            f"Add any user with `@{org.domain_name}` email to this organization."
-        )
+    render_org_create_or_edit_form(org=org)
 
     if st.button("Save", className="w-100", type="primary"):
         try:
@@ -218,15 +190,16 @@ def render_org_leave_view_by_membership(
             for m in org.memberships.all().select_related("user")
             if m != current_membership
         }
+
+        st.caption(
+            "You are the only owner of this organization. Please choose another member to promote to owner."
+        )
         new_owner_uid = st.selectbox(
             "New Owner",
             options=list(members_by_uid),
             format_func=lambda uid: format_user_name(members_by_uid[uid].user),
         )
         new_owner = members_by_uid[new_owner_uid]
-        st.caption(
-            "You are the only owner of this organization. Please choose another member to promote to owner."
-        )
 
     with st.div(className="d-flex"):
         if st.button(
@@ -357,8 +330,34 @@ def render_invite_creation_view(org: Org, inviter: AppUser, modal: Modal):
             modal.close()
 
 
+def render_org_create_or_edit_form(org: Org | None = None) -> AttrDict | Org:
+    org_proxy = org or AttrDict()
+
+    org_proxy.name = st.text_input("Team Name", value=org and org.name or "")
+    org_proxy.logo = st.file_uploader(
+        "Logo", accept=["image/*"], value=org and org.logo or ""
+    )
+    org_proxy.domain_name = st.text_input(
+        "Domain Name (Optional)",
+        placeholder="e.g. gooey.ai",
+        value=org and org.domain_name or "",
+    )
+    if org_proxy.domain_name:
+        st.caption(
+            f"Invite any user with `@{org_proxy.domain_name}` email to this organization."
+        )
+
+    return org_proxy
+
+
 def format_user_name(user: AppUser, current_user: AppUser | None = None):
     name = user.display_name or user.first_name()
     if current_user and user == current_user:
         name += " (You)"
     return name
+
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__dict__ = self
