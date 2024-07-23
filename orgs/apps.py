de6ed2f156025f0e5c87e1d586a1162f61cf43be
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-07-23 17:10:44 +0530

    add invitation page

--- orgs/apps.py
+++ orgs/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class OrgsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "orgs"
+
+    def ready(self):
+        from . import signals
+
+        assert signals
