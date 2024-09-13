76fe9aa8fe912267ac12fa24c2f785e268983467
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-09-13 14:48:26 +0530

    Refactor GooeyEnum to separate .name and .api_value
    We have a different name vs api_value for SerpSearchLocation

--- daras_ai_v2/custom_enum.py
+++ daras_ai_v2/custom_enum.py
@@ -1,12 +1,38 @@
+import functools
 import typing
 from enum import Enum
 
 import typing_extensions
 
+
+def cached_classmethod(func: typing.Callable):
+    """
+    This cache is a hack to get around a bug where
+    dynamic Enums with the same name cause a crash
+    when generating the OpenAPI spec.
+    """
+
+    @functools.wraps(func)
+    def wrapper(cls):
+        if not hasattr(cls, "_cached_classmethod"):
+            cls._cached_classmethod = {}
+        if id(func) not in cls._cached_classmethod:
+            cls._cached_classmethod[id(func)] = func(cls)
+
+        return cls._cached_classmethod[id(func)]
+
+    return wrapper
+
+
 T = typing.TypeVar("T", bound="GooeyEnum")
 
 
 class GooeyEnum(Enum):
+    @property
+    def api_value(self):
+        # api_value is usually the name
+        return self.name
+
     @classmethod
     def db_choices(cls):
         return [(e.db_value, e.label) for e in cls]
@@ -21,34 +47,27 @@ class GooeyEnum(Enum):
     @classmethod
     @property
     def api_choices(cls):
-        return typing.Literal[tuple(e.name for e in cls)]
+        return typing.Literal[tuple(e.api_value for e in cls)]
 
     @classmethod
     @property
+    @cached_classmethod
     def api_enum(cls):
         """
         Enum that is useful as a type in API requests.
 
-        Maps `name`->`name` for all values.
+        Maps `api_value`->`api_value` (default: `name`->`name`)
+        for all values.
+
         The title (same as the Enum class name) will be
         used as the new Enum's title. This will be passed
         on to the OpenAPI schema and the generated SDK.
         """
-        # this cache is a hack to get around a bug where
-        # dynamic Enums with the same name crash when
-        # generating the OpenAPI spec
-        if not hasattr(cls, "_cached_api_enum"):
-            cls._cached_api_enum = {}
-        if cls.__name__ not in cls._cached_api_enum:
-            cls._cached_api_enum[cls.__name__] = Enum(
-                cls.__name__, {e.name: e.name for e in cls}
-            )
-
-        return cls._cached_api_enum[cls.__name__]
+        return Enum(cls.__name__, {e.api_value: e.api_value for e in cls})
 
     @classmethod
-    def from_api(cls, name: str) -> typing_extensions.Self:
+    def from_api(cls, api_value: str) -> typing_extensions.Self:
         for e in cls:
-            if e.name == name:
+            if e.api_value == api_value:
                 return e
-        raise ValueError(f"Invalid {cls.__name__} {name=}")
+        raise ValueError(f"Invalid {cls.__name__} {api_value=}")
