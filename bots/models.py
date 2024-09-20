ac75f6e16d20698c6a06bafba7d14b4c078df82c
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-09-21 00:53:29 +0530

    Add workspace to published run

--- bots/models.py
+++ bots/models.py
@@ -1714,6 +1714,12 @@ class PublishedRun(models.Model):
         null=True,
     )
 
+    workspace = models.ForeignKey(
+        "workspaces.Workspace",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
