7fb01b6cd36bc0512d24ceadf0a80dcf26ea3df3
anish-work <anish.saxenaa@gmail.com>
2024-10-25 03:04:28 +0530

    replace checkbox with switch component

--- functions/recipe_functions.py
+++ functions/recipe_functions.py
@@ -188,12 +188,13 @@ def functions_input(current_user: AppUser, key="functions"):
             )
         col2.node.children[0].props["className"] += " col-12"
 
-    if gui.checkbox(
+    if gui.switch(
         f"##### {field_title_desc(BasePage.RequestModel, key)}",
         key=f"--enable-{key}",
         value=key in gui.session_state,
+        className="mb-2",
     ):
-        with gui.div(className="ms-4 ps-1"):
+        with gui.div(className="ps-1"):
             gui.session_state.setdefault(key, [{}])
             with gui.div(className="d-flex align-items-center"):
                 gui.write("###### Functions")
