7a2ebbeb75837ffc0e206a03346480ea91e930fe
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-10-29 15:08:53 +0530

    Add migration script for bot integrations

--- scripts/migrate_workspaces.py
+++ scripts/migrate_workspaces.py
@@ -7,7 +7,7 @@ from django.db.models import OuterRef, Subquery
 
 from api_keys.models import ApiKey
 from app_users.models import AppUser, AppUserTransaction
-from bots.models import SavedRun, PublishedRun
+from bots.models import BotIntegration, SavedRun, PublishedRun
 from daras_ai_v2 import db
 from workspaces.models import Workspace, WorkspaceMembership, WorkspaceRole
 
@@ -23,6 +23,7 @@ def run():
     migrate_saved_runs()
     migrate_published_runs()
     migrate_api_keys()
+    migrate_bot_integrations()
 
 
 @transaction.atomic
@@ -130,6 +131,21 @@ def migrate_api_keys():
         )
         print(total, end=SEP)
         total += len(migrated_keys)
+
+
+def migrate_bot_integrations():
+    qs = BotIntegration.objects.filter(
+        workspace__isnull=True,
+        billing_account_uid__isnull=False,
+    )
+    print(f"migrating {qs.count()} bot integrations", end=SEP)
+    update_in_batches(
+        qs,
+        workspace_id=Workspace.objects.filter(
+            is_personal=True,
+            created_by__uid=OuterRef("billing_account_uid"),
+        ).values("id")[:1],
+    )
 
 
 def update_in_batches(qs, **kwargs):
