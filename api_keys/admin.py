dc7bcf16c0ed82b90e1c433beb3d6f7cff77b14b
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-10-29 14:15:06 +0530

    Add search fields for API keys admin

--- api_keys/admin.py
+++ api_keys/admin.py
@@ -6,3 +6,10 @@ from .models import ApiKey
 @admin.register(ApiKey)
 class ApiKeyAdmin(admin.ModelAdmin):
     list_display = ("preview", "workspace", "created_by", "created_at")
+    search_fields = [
+        "preview",
+        "workspace__name",
+        "created_by__display_name",
+        "created_by__email",
+        "created_by__uid",
+    ]
