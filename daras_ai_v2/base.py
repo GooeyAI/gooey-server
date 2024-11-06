27f0bfc5fd26cf3e203bbf529f3c1f48c14f32a9
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-11-06 17:51:33 +0530

    remove stale uid and workspace_id variable assignment in history tab

--- daras_ai_v2/base.py
+++ daras_ai_v2/base.py
@@ -2177,12 +2177,6 @@ We’re always on <a href="{settings.DISCORD_INVITE_URL}" target="_blank">discor
     def _history_tab(self):
         self.ensure_authentication(anon_ok=True)
 
-        workspace_id = self.current_workspace.id
-        uid = self.request.user.uid
-        if self.is_current_user_admin():
-            uid = self.request.query_params.get("uid", uid)
-            workspace_id = self.request.query_params.get("workspace_id", workspace_id)
-
         before = self.request.query_params.get("updated_at__lt", None)
         if before:
             before = datetime.datetime.fromisoformat(before)
