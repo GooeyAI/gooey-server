1c32b3eca71d6394b4bf0c9825cf193c3b1d68f2
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-11-14 16:29:41 +0530

    Revert "remove feature to auto-add members with matching domain name without invitation"
    This reverts commit 94e720da63c0f658e47ac4aedaf1e061e11168ed.

--- workspaces/signals.py
+++ workspaces/signals.py
@@ -1,8 +1,26 @@
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from loguru import logger
 from safedelete.signals import post_softdelete
 
-from .models import WorkspaceMembership
+from app_users.models import AppUser
+from .models import Workspace, WorkspaceMembership
+from .tasks import send_added_to_workspace_email
+
+
+@receiver(post_save, sender=AppUser)
+def add_user_existing_workspace(instance: AppUser, **kwargs):
+    """
+    if the domain name matches
+    """
+    if not instance.email:
+        return
+    email_domain = instance.email.split("@")[-1].lower()
+    for workspace in Workspace.objects.filter(domain_name=email_domain):
+        WorkspaceMembership.objects.get_or_create(workspace=workspace, user=instance)
+        send_added_to_workspace_email.delay(
+            workspace_id=workspace.id, user_id=instance.id
+        )
 
 
 @receiver(post_softdelete, sender=WorkspaceMembership)
