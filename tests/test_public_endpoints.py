b5e9822728103ec115a409899c840c3949203517
Dev Aggarwal <devxpy@gmail.com>
2024-12-01 00:27:27 +0530

    add tests for stats route check for published run title in endpoint test

--- tests/test_public_endpoints.py
+++ tests/test_public_endpoints.py
@@ -3,9 +3,18 @@ import random
 from starlette.routing import Route
 from starlette.testclient import TestClient
 
-from bots.models import Workflow, PublishedRun
+from bots.models import (
+    Workflow,
+    PublishedRun,
+    BotIntegration,
+    Message,
+    Conversation,
+    CHATML_ROLE_USER,
+    CHATML_ROLE_ASSISSTANT,
+)
+from daras_ai_v2.fastapi_tricks import get_route_path
 from routers import facebook_api
-from routers.root import RecipeTabs
+from routers.root import RecipeTabs, integrations_stats_route
 from routers.slack_api import slack_connect_redirect_shortcuts, slack_connect_redirect
 from routers.static_pages import webflow_upload
 from server import app
@@ -43,23 +52,60 @@ def _test_get_path(path):
     assert r.ok, r.content
 
 
+def test_integration_stats_route(db_fixtures, force_authentication, threadpool_subtest):
+    for bi in BotIntegration.objects.all():
+        for i in range(5):
+            convo = Conversation.objects.create(
+                bot_integration=bi, web_user_id=f"test-user-id-{i%2}"
+            )
+            for j in range(10):
+                Message.objects.create(
+                    conversation=convo,
+                    role=CHATML_ROLE_USER,
+                    content="test-message-1",
+                )
+                Message.objects.create(
+                    conversation=convo,
+                    role=CHATML_ROLE_ASSISSTANT,
+                    content="test-message-2",
+                )
+        url_path = get_route_path(
+            integrations_stats_route,
+            path_params=dict(
+                page_slug="test-slug",
+                integration_id=bi.api_integration_id(),
+            ),
+        )
+        threadpool_subtest(
+            _test_post_path,
+            url_path,
+            "2 Active Users",
+            "5 Conversations",
+            "100 Total Messages",
+        )
+
+
 def test_all_post(db_fixtures, force_authentication, threadpool_subtest):
     for pr in PublishedRun.objects.all():
         for tab in RecipeTabs:
             slug = random.choice(Workflow(pr.workflow).page_cls.slug_versions)
-            threadpool_subtest(
-                _test_post_path,
-                tab.url_path(slug, "test-run-slug", pr.published_run_id),
-            )
+            url_path = tab.url_path(slug, "test-run-slug", pr.published_run_id)
+            if RecipeTabs in [RecipeTabs.run, RecipeTabs.run_as_api]:
+                test_content = [pr.title]
+            else:
+                test_content = []
+            threadpool_subtest(_test_post_path, url_path, *test_content)
 
 
-def _test_post_path(url):
+def _test_post_path(url, *test_content):
     for _ in range(10):
         r = client.post(url, json={}, allow_redirects=False)
         if r.is_redirect:
             url = r.headers["Location"]
             continue
         assert r.ok, r.content
+        for expected in test_content:
+            assert expected in str(r.json()), str(r.json())
         return
     else:
         assert False, f"Too many redirects: {url}"
