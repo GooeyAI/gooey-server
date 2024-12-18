a1e9c8599f75facabfaf077b39105f48c89e9492
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2024-12-18 16:25:36 +0530

    add 25K to max tokens chosen by user for o1 models

--- daras_ai_v2/language_model.py
+++ daras_ai_v2/language_model.py
@@ -1054,6 +1054,10 @@ def run_openai_chat(
         max_tokens = max_completion_tokens
         max_completion_tokens = NOT_GIVEN
 
+        # reserved tokens for reasoning...
+        # https://platform.openai.com/docs/guides/reasoning#allocating-space-for-reasoning
+        max_completion_tokens += 25_000
+
     if avoid_repetition:
         frequency_penalty = 0.1
         presence_penalty = 0.25
