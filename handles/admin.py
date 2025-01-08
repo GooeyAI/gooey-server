b258817e210657fbf697e8dd836a9feabf76dcbe
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2025-01-08 20:51:54 +0530

    add handle<>workspace connection to admin pages

--- handles/admin.py
+++ handles/admin.py
@@ -1,17 +1,21 @@
 from django.contrib import admin
 
 from app_users.admin import AppUserAdmin
+from workspaces.admin import WorkspaceAdmin
 from .models import Handle
 
 
 @admin.register(Handle)
 class HandleAdmin(admin.ModelAdmin):
-    search_fields = ["name", "redirect_url"] + [
-        f"user__{field}" for field in AppUserAdmin.search_fields
-    ]
-    readonly_fields = ["user", "created_at", "updated_at"]
+    search_fields = (
+        ["name", "redirect_url"]
+        + [f"user__{field}" for field in AppUserAdmin.search_fields]
+        + [f"workspace__{field}" for field in WorkspaceAdmin.search_fields]
+    )
+    readonly_fields = ["user", "workspace", "created_at", "updated_at"]
 
     list_filter = [
         ("user", admin.EmptyFieldListFilter),
+        ("workspace", admin.EmptyFieldListFilter),
         ("redirect_url", admin.EmptyFieldListFilter),
     ]
