a71b711ce83c8a91cdfa86c416d365c5ad6885d3
Dev Aggarwal <devxpy@gmail.com>
2025-02-06 18:29:26 -0800

    Allow null search queries to skip performing vector search

--- recipes/VideoBots.py
+++ recipes/VideoBots.py
@@ -1010,7 +1010,7 @@ PS. This is the workflow that we used to create RadBots - a collection of Turing
                     response=response,
                     instructions=query_instructions,
                     context={"messages": chat_history},
-                )
+                ).strip()
             else:
                 query_msgs.reverse()
                 response.final_search_query = "\n---\n".join(
@@ -1037,18 +1037,19 @@ PS. This is the workflow that we used to create RadBots - a collection of Turing
                     keyword_query = list(keyword_query.values())[0]
                 response.final_keyword_query = keyword_query
 
-            # perform doc search
-            response.references = yield from get_top_k_references(
-                DocSearchRequest.parse_obj(
-                    {
-                        **request.dict(),
-                        **response.dict(),
-                        "search_query": response.final_search_query,
-                        "keyword_query": response.final_keyword_query,
-                    },
-                ),
-                current_user=self.request.user,
-            )
+            if response.final_search_query:
+                # perform doc search
+                response.references = yield from get_top_k_references(
+                    DocSearchRequest.parse_obj(
+                        {
+                            **request.dict(),
+                            **response.dict(),
+                            "search_query": response.final_search_query,
+                            "keyword_query": response.final_keyword_query,
+                        },
+                    ),
+                    current_user=self.request.user,
+                )
             if request.use_url_shortener:
                 for reference in response.references:
                     reference["url"] = ShortenedURL.objects.get_or_create_for_workflow(
