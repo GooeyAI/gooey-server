78d39d30ee9dc7e5aed8eef4753be3be3a71f6a2
anish-work <anish.saxenaa@gmail.com>
2025-02-12 12:16:00 +0530

    add photo_url to PublishedRuns

--- bots/models.py
+++ bots/models.py
@@ -1904,6 +1904,7 @@ class PublishedRun(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     objects = PublishedRunQuerySet.as_manager()
+    photo_url = CustomURLField(default="", blank=True)
 
     class Meta:
         get_latest_by = "updated_at"
