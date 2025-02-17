ece58532b201a1501dbe34ad09776352342b033c
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2025-02-17 19:30:22 +0530

    add new fixture file without app_users.handle

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,7 +3,7 @@
 set -ex
 
 echo "==> Downloading fixture.json..."
-wget -N -nv https://storage.googleapis.com/dara-c1b52.appspot.com/daras_ai/media/2f4f3e7c-e3bf-11ef-b200-02420a0001cf/fixture.json
+wget -N -nv https://storage.googleapis.com/dara-c1b52.appspot.com/daras_ai/media/6cfdb56a-ed37-11ef-9b59-02420a000160/fixture.json
 
 echo "==> Linting with black..."
 black --check --diff .
