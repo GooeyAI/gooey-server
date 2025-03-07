ab33fd2d2108a6c6ba458f514a533089fbf8ef34
Dev Aggarwal <devxpy@gmail.com>
2025-03-06 17:33:16 -0800

    update fixtures

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,7 +3,7 @@
 set -ex
 
 echo "==> Downloading fixture.json..."
-wget -N -nv https://storage.googleapis.com/dara-c1b52.appspot.com/daras_ai/media/177258a2-fadf-11ef-8ffe-02420a00013c/fixture.json
+wget -N -nv https://storage.googleapis.com/dara-c1b52.appspot.com/daras_ai/media/b1f4d3b4-faf3-11ef-a62c-02420a00013c/fixture.json
 
 echo "==> Linting with black..."
 black --check --diff .
