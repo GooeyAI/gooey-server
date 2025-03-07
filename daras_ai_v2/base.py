83d302902252d089ef056459190472c9a3be692c
anish-work <anish.saxenaa@gmail.com>
2025-03-07 17:20:40 +0530

    redner_example: remove kwargs from the function

--- daras_ai_v2/base.py
+++ daras_ai_v2/base.py
@@ -2275,7 +2275,8 @@ class BasePage:
         self,
         published_run: PublishedRun,
         use_workspace_author: bool = False,
-        **kwargs,
+        workflow_pill: str | None = None,
+        hide_visibility_pill: bool = False,
     ):
         max_desc_words = 280
         max_desc_words_mobile = 100
@@ -2283,19 +2284,19 @@ class BasePage:
         version = published_run.versions.latest()
         pills = [
             lambda: (
-                gui.pill(
+                None
+                if hide_visibility_pill
+                else gui.pill(
                     PublishedRunVisibility(published_run.visibility).get_badge_html(),
                     unsafe_allow_html=True,
                     className="border border-dark",
                 )
-                if not "hide_visibility_pill" in kwargs
-                else None
             ),
         ]
-        if kwargs.get("workflow_pill"):
+        if workflow_pill:
             pills.append(
                 lambda: gui.pill(
-                    kwargs["workflow_pill"],
+                    workflow_pill,
                     unsafe_allow_html=True,
                     className="border border-dark ms-2",
                 )
@@ -2458,7 +2459,6 @@ class BasePage:
     ):
         self.render_example(
             published_run,
-            hide_author_column=True,
             use_workspace_author=True,
             hide_visibility_pill=True,
         )
