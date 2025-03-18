87dbaf02316f9c3639c8fe7d07fb4c6a2cae19c7
anish-work <anish.saxenaa@gmail.com>
2025-03-18 18:40:36 +0530

    remove hide button from examples

--- daras_ai_v2/base.py
+++ daras_ai_v2/base.py
@@ -2448,18 +2448,6 @@ class BasePage:
             hide_visibility_pill=True,
             hide_version_notes=True,
         )
-        if allow_hide:
-            self._example_hide_button(published_run=published_run)
-
-    def _example_hide_button(self, published_run: PublishedRun):
-        pressed_delete = gui.button(
-            "🙈️ Hide",
-            key=f"delete_example_{published_run.published_run_id}",
-            style={"color": "red"},
-        )
-        if not pressed_delete:
-            return
-        self.set_hidden(published_run=published_run, hidden=True)
 
     def set_hidden(self, *, published_run: PublishedRun, hidden: bool):
         with gui.spinner("Hiding..."):
