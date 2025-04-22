98b1fc33923ab4546f77a208da26ca9361be9e48
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2025-04-22 18:27:06 +0530

    fix if-else case for deprecated->redirect vs deprecated->error

--- daras_ai_v2/language_model.py
+++ daras_ai_v2/language_model.py
@@ -878,24 +878,24 @@ def run_language_model(
 
     model: LargeLanguageModels = LargeLanguageModels[str(model)]
 
-    if model.is_deprecated:
-        if model.redirect_to:
-            return run_language_model(
-                prompt=prompt,
-                messages=messages,
-                model=model.redirect_to,
-                max_tokens=max_tokens,
-                num_outputs=num_outputs,
-                temperature=temperature,
-                stop=stop,
-                avoid_repetition=avoid_repetition,
-                tools=tools,
-                response_format_type=response_format_type,
-                stream=stream,
-                audio_url=audio_url,
-                audio_session_extra=audio_session_extra,
-            )
-            raise UserError(f"Model {model} is deprecated.")
+    if model.is_deprecated and model.redirect_to:
+        return run_language_model(
+            prompt=prompt,
+            messages=messages,
+            model=model.redirect_to,
+            max_tokens=max_tokens,
+            num_outputs=num_outputs,
+            temperature=temperature,
+            stop=stop,
+            avoid_repetition=avoid_repetition,
+            tools=tools,
+            response_format_type=response_format_type,
+            stream=stream,
+            audio_url=audio_url,
+            audio_session_extra=audio_session_extra,
+        )
+    elif model.is_deprecated:
+        raise UserError(f"Model {model} is deprecated.")
 
     if model.max_output_tokens:
         max_tokens = min(max_tokens, model.max_output_tokens)
