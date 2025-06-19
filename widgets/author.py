da715819e65b00a5a4df4d1b2d58fb295059941b
Avinash Anand Lal <avinashanand.dav@gmail.com>
2025-06-19 14:37:09 -0700

    Update author link rendering to remove underline

--- widgets/author.py
+++ widgets/author.py
@@ -42,7 +42,7 @@ def render_author_as_breadcrumb(
                 full_name = "Deleted User"
                 link = None
 
-            linkto = link and gui.link(to=link) or gui.dummy()
+            linkto = link and gui.link(to=link, className="text-decoration-none") or gui.dummy()
             with linkto:
                 gui.caption(full_name)
 
@@ -114,7 +114,7 @@ def render_author(
     else:
         responsive_image_size = image_size
 
-    linkto = link and gui.link(to=link) or gui.dummy()
+    linkto = link and gui.link(to=link, className="text-decoration-none") or gui.dummy()
     with linkto, gui.div(className="d-flex align-items-center"):
         if photo:
             with gui.styled(
