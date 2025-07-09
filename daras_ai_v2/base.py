d7bb368b9136e8ba76f519631ce875e698c24592
Anish <anish.saxenaa@gmail.com>
2025-07-09 14:10:44 +0530

    fix: run tab underline

--- daras_ai_v2/base.py
+++ daras_ai_v2/base.py
@@ -453,10 +453,7 @@ class BasePage:
                 with gui.tag(
                     "span",
                     className="active-lg"
-                    if (
-                        tab == RecipeTabs.run
-                        and self.workflow in PREVIEW_ROUTE_WORKFLOWS
-                    )
+                    if (tab == RecipeTabs.run and self.tab == RecipeTabs.preview)
                     else "",
                 ):
                     with gui.nav_item(
