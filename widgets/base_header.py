210e04b312c5cb6be62c8ff1e5299606214a0305
Anish <anish.saxenaa@gmail.com>
2025-07-10 15:14:24 +0530

    header: show inline demo buttons and breadcrumbs

--- widgets/base_header.py
+++ widgets/base_header.py
@@ -2,6 +2,16 @@ import gooey_gui as gui
 
 from bots.models import Workflow
 from daras_ai_v2 import icons
+from widgets.author import (
+    render_author_as_breadcrumb,
+)
+from app_users.models import AppUser
+from daras_ai_v2.breadcrumbs import render_breadcrumbs
+from bots.models import (
+    PublishedRun,
+    SavedRun,
+)
+from workspaces.models import Workspace
 
 
 def render_help_button(workflow: Workflow):
@@ -20,3 +30,32 @@ def render_help_button(workflow: Workflow):
         gui.html(
             f'{icons.help_guide} <span class="text-muted d-none d-lg-inline">Help Guide</span>'
         )
+
+
+def render_breadcrumbs_with_author(
+    tbreadcrumbs,
+    *,
+    is_api_call: bool = False,
+    is_root_example: bool = False,
+    user: AppUser | None = None,
+    pr: PublishedRun | None = None,
+    sr: SavedRun | None = None,
+    current_workspace: Workspace | None = None,
+):
+    with gui.div(
+        className="d-flex flex-wrap align-items-center container-margin-reset"
+    ):
+        with gui.div(className="me-2"):
+            render_breadcrumbs(
+                tbreadcrumbs,
+                is_api_call=is_api_call,
+            )
+        if not is_root_example:
+            with gui.div(className="d-flex align-items-center"):
+                gui.write("by", className="me-2 d-block text-muted")
+                render_author_as_breadcrumb(
+                    user=user,
+                    pr=pr,
+                    sr=sr,
+                    current_workspace=current_workspace,
+                )
