13ba8d61ecc4f288731135b2eb7f35816212139c
Dev Aggarwal <devxpy@gmail.com>
2026-01-09 14:45:05 +0530

    update fixtures

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,7 +3,7 @@
 set -ex
 
 echo "==> Downloading fixture.json..."
-wget -N -nv https://storage.googleapis.com/dara-c1b52.appspot.com/daras_ai/media/63fdbbbe-9fbf-11f0-9ffd-02420a000184/fixture.json
+wget -N -nv https://storage.googleapis.com/dara-c1b52.appspot.com/daras_ai/media/f0769d1a-ed38-11f0-96f2-02420a0001b0/fixture.json
 
 echo "==> Formatting with ruff..."
 poetry run ruff format --diff .
