2afed64d5e83a300c6d63d86a7b14559277fad7c
Anish <anish.saxenaa@gmail.com>
2026-06-02 18:21:40 +0530

    fix: use DateTime field for publish date in News Item

--- cms/models.py
+++ cms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 from bots.custom_fields import CustomURLField
 
@@ -76,15 +77,11 @@ class NewsItem(models.Model):
         help_text='Freeform label, e.g. "Event", "Product", "Case Study".',
     )
     photo_url = CustomURLField(default="", blank=True)
-    publish_date = models.DateField()
+    publish_date = models.DateTimeField(default=timezone.now)
     url = CustomURLField(default="", blank=False, help_text="News item URL.")
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ["-publish_date", "-id"]
-        indexes = [models.Index(fields=["-publish_date"])]
 
     def __str__(self):
         return self.headline
